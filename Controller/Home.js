1dd87b54bf20d323c6364c235e2bc41f31eceb0c
Pete LePage <pete.lepage@pobox.com>
2014-08-26 14:54:25 -0400

    home rewrite

--- Controller/Home.js
+++ Controller/Home.js
@@ -1,293 +1,279 @@
 var EventEmitter = require("events").EventEmitter;
-var GoogleVoice = require("./GoogleVoice");
-var Hue = require("./Hue");
-var Door = require("./Door");
-var InsideTemperature = require("./InsideTemperature");
-var Harmony = require("./Harmony");
-var AirConditioner = require("./AirConditioner");
-var Firebase = require("firebase");
-var fs = require("fs");
-var sys = require("sys");
-var exec = require("child_process").exec;
 var util = require("util");
+var exec = require("child_process").exec;
 var log = require("./SystemLog");
 var Keys = require("./Keys");
 
-var fb, hue, harmony, airConditioners;
+var InsideTemperature = require("./InsideTemperature");
+var Firebase = require("firebase");
+var AirConditioner = require("./AirConditioner");
+var Harmony = require("./Harmony");
+var Hue = require("./Hue");
+var Door = require("./Door");
+var GoogleVoice = require("./GoogleVoice");
 
-function Home() {
-  this.state = {
-    "system_state": "STARTING",
-    "hue": {},
-    "door": -1,
-    "ac": {},
-    "temperature": {
-      "inside": -1,
-      "outside": -1
-    },
-    "time": {
-      "started": Date.now()
-    },
-    "harmony_activity": -1,
-    "gvoice": {}
+function Home(fb) {
+  this.state = {};
+  this.errors = [];
+  this.harmonyConfig = {};
+  var ready = false;
+  var config;
+  var _self = this;
+
+  var awayTimer;
+  var hue, harmony, airConditioners, insideTemp, door, gv;
+
+  this.set = function(command, options) {
+    log.log("Command Received: " + command + " " + "[" + options + "]");
+    var cmd = config.commands[command];
+    if (cmd) {
+      if (cmd.system_state) {
+        setState(cmd.system_state);
+      }
+      if (cmd.hue) {
+        for (var i = 0; i < cmd.hue.length; i++) {
+          var hue_cmd = options || cmd.hue[i].command;
+          hue_cmd = config.light_recipes[hue_cmd];
+          hue.setLights(cmd.hue[i].lights, hue_cmd);
+        }
+      }
+      if (cmd.ac) {
+        var acKeys = Object.keys(cmd.ac);
+        for (var i = 0; i < acKeys.length; i++) {
+          var temperature = cmd.ac[acKeys[i]];
+          _self.setACTemperature(acKeys[i], temperature);
+        }
+      }
+      if (cmd.harmony) {
+        harmony.startActivity(cmd.harmony);
+      }
+      if (cmd.sound) {
+        playSound(cmd.sound);
+      }
+    }
+    return {"result": "OK"};
   };
-  this.config = null;
-  readConfig(this);
-}
 
-util.inherits(Home, EventEmitter);
-
-Home.prototype.shutdown = function() {
-  // TODO: shutdown harmony connection
-};
-
-Home.prototype.set = function(command, options) {
-  log.log("Command Received: " + command + " " + "[" + options + "]");
-  var cmd = this.config.commands[command];
-  if (cmd) {
-    if (cmd.system_state) {
-      setState(cmd.system_state, this);
-    }
-    if (cmd.hue) {
-      for (var i = 0; i < cmd.hue.length; i++) {
-        var hue_cmd = options || cmd.hue[i].command;
-        hue_cmd = this.config.light_recipes[hue_cmd];
-        hue.setLights(cmd.hue[i].lights, hue_cmd);
+  this.setTemperature = function(id, temperature) {
+    log.log("Set AC [" + id + "] to " + temperature + "F");
+    if (temperature === "AUTO") {
+      if ((_self.state.temperature.inside >= config.airconditioners.auto.inside) ||
+          (_self.state.temperature.outside >= config.airconditioners.auto.outside)) {
+        temperature = config.airconditioners.default_temperature;
+      } else {
+        temperature = null;
       }
     }
-    if (cmd.ac) {
-      var acKeys = Object.keys(cmd.ac);
-      for (var i = 0; i < acKeys.length; i++) {
-        var temperature = cmd.ac[acKeys[i]];
-        this.setACTemperature(acKeys[i], temperature);
+    if (temperature !== null) {
+      airConditioners[id].setTemperature(temperature);
+      _self.state.ac[id] = temperature;
+      fbSet("state/ac/" + id, temperature);
+    }
+    return {"result": "OK"};
+  };
+
+  this.shutdown = function() {
+    harmony.close();
+  };
+
+  function fbPush(path, value) {
+    var fbObj = fb;
+    if (path) {
+      fbObj = fb.child(path);
+    }
+    fb.child("state/time").update({"last_updated": Date.now()});
+    fbObj.push(value);
+  }
+
+  function fbSet(path, value) {
+    var fbObj = fb;
+    if (path) {
+      fbObj = fb.child(path);
+    }
+    fb.child("state/time").update({"last_updated": Date.now()});
+    fbObj.set(value);
+  }
+
+  function setState(state) {
+    log.log("Set State: " + state);
+    if (state === "ARMED") {
+      if (awayTimer) {
+        clearTimeout(awayTimer);
       }
+      awayTimer = setTimeout(function() {
+        awayTimer = null;
+        setState("AWAY");
+      }, config.away_timeout);
     }
-    if (cmd.harmony) {
-      harmony.startActivity(cmd.harmony);
+    _self.state.system_state = state;
+    fbSet("state/system_state", state);
+    fbPush("system_state", {"date": Date.now(), "state": state});
+  }
+
+  function playSound(file) {
+    setTimeout(function() {
+      var cmd = "mplayer -ao alsa -really-quiet -noconsolecontrols ";
+      cmd += file;
+      exec(cmd, function(error, stdout, stderr) {
+        if (error !== null) {
+          log.error("[HOME] PlaySound Error: " + error.toString());
+        }
+      });
+      log.debug("PlaySound: " + file);
+    }, 1);
+  }
+
+  function initInsideTemp() {
+    insideTemp = new InsideTemperature(config.temperature.inside.interval);
+    if (_self.state.temperature === undefined) {
+      _self.state.temperature = {};
     }
-    if (cmd.sound) {
-      playSound(cmd.sound);
+    insideTemp.on("error", function(error) {
+      _self.state.temperature.inside = -1;
+      fbSet("state/temperature/inside", error);
+      log.error("[HOME] Error reading inside temperature: " + error);
+    });
+    insideTemp.on("update", function(data) {
+      _self.state.temperature.inside = data.f;
+      fbSet("state/temperature/inside", data.f);
+      log.debug("[HOME] Inside temperature is " + data.f + "F");
+    });
+  }
+
+  function initOutsideTemp() {
+    if (_self.state.temperature === undefined) {
+      _self.state.temperature = {};
     }
+    var url = "https://publicdata-weather.firebaseio.com/";
+    url += config.temperature.outside.city + "/currently";
+    var weatherRef = new Firebase(url);
+    weatherRef.child('temperature').on('value', function(snapshot) {
+      _self.state.temperature.outside = snapshot.val();
+      fbSet("state/temperature/outside", snapshot.val());
+      log.debug("[HOME] Outside temperature is " + snapshot.val() + "F");
+    });
   }
-  return {"result": "OK"};
-};
 
-Home.prototype.setACTemperature = function(id, temperature) {
-  log.log("Set AC [" + id + "] to " + temperature + "F");
-  if (temperature === "AUTO") {
-    if ((this.state.temperature.inside >= this.config.airconditioners.auto.inside) ||
-        (this.state.temperature.outside >= this.config.airconditioners.auto.outside)) {
-      temperature = this.config.airconditioners.default_temperature;
-    } else {
-      temperature = null;
-    }
+  function initAC() {
+    airConditioners = {};
+    _self.state.ac = {};
+    var ip = config.airconditioners.itach_ip;
+    var port = config.airconditioners.itach_port;
+    config.airconditioners.ac.forEach(function(elem) {
+      var id = elem.id;
+      var cmds = config.airconditioners.commands[elem.protocol];
+      airConditioners[id] = new AirConditioner(id, ip, port, cmds);
+      _self.state.ac[id] = 0;
+      fbSet("state/ac/" + id, 0);
+    });
   }
-  if (temperature !== null) {
-    airConditioners[id].setTemperature(temperature);
-    this.state.ac[id] = temperature;
-    fbSet("state/ac/" + id, temperature);
+
+  function initHarmony() {
+    harmony = new Harmony(config.harmony.ip, Keys.keys.harmony);
+    harmony.on("activity", function(activity) {
+      _self.state.harmony_activity = activity;
+      fbSet("state/harmony_activity", activity);
+      log.debug("[HOME] Harmony activity changed: " + activity);
+    });
+    harmony.on("config", function(cfg) {
+      _self.harmonyConfig = cfg;
+      fbSet("harmony_config", cfg);
+    });
+    harmony.on("error", function(err) {
+      log.error("[HOME] Harmony Error");
+      log.debug(err);
+    });
+    harmony.getConfig();
   }
-  return {"result": "OK"};
-};
 
-function fbPush(path, value) {
-  var fbObj = fb;
-  if (path) {
-    fbObj = fb.child(path);
+  function initHue() {
+    hue = new Hue(config.hue.interval, Keys.keys.hue, config.hue.ip);
+    hue.on("update", function(data) {
+      _self.state.hue = data;
+      fbSet("state/hue", data);
+    });
+    hue.on("error", function (err) {
+      var error = {"error": true, "result": err};
+      _self.state.hue = error;
+      fbSet("state/hue", error);
+      log.error("[HOME] Error reading Hue state.");
+      log.debug(JSON.stringify(err));
+    });
   }
-  fb.child("state/time").update({"last_updated": Date.now()});
-  fbObj.push(value);
-}
 
-function fbSet(path, value) {
-  var fbObj = fb;
-  if (path) {
-    fbObj = fb.child(path);
+  function initDoor() {
+    _self.state.door = false;
+    door = new Door();
+    door.on("no-gpio", function() {
+      _self.state.door = undefined;
+      fbSet("state/door", null);
+      log.error("[HOME] No GPIO found for door detection.");
+    });
+    door.on("changed", function(data) {
+      if (_self.state.system_state === "AWAY") {
+        _self.set("HOME");
+      }
+      _self.state.door = data;
+      fbSet("state/door", data);
+      fbPush("door", {"date": Date.now(), "state": data});
+    });
   }
-  fb.child("state/time").update({"last_updated": Date.now()});
-  fbObj.set(value);
-}
 
-var awayTimeout;
-function setState(state, self) {
-  log.log("Set State: " + state);
-  if (state === "ARMED") {
-    if (awayTimeout) {
-      clearTimeout(awayTimeout);
-    }
-    awayTimeout = setTimeout(function() {
-      awayTimeout = null;
-      setState("AWAY", self);
-    }, self.config.away_timeout);
-  }
-  self.state.system_state = state;
-  fbSet("state/system_state", state);
-  fbPush("system_state", {"date": Date.now(), "state": state});
-}
-
-function readConfig(self) {
-  log.log("Reading config files.");
-  fs.readFile("config.json", {"encoding": "utf8"}, function(err, data) {
-    if (err) {
-      log.error("[HOME] Unable to read config.json");
-    } else {
-      self.config = JSON.parse(data);
-      log.log("[HOME] config.json read and parsed");
-      onReady(self);
-    }
-  });
-}
-
-var playSound = function(file) {
-  setTimeout(function() {
-    var cmd = "mplayer -ao alsa -really-quiet -noconsolecontrols ";
-    cmd += file;
-    exec(cmd, function(error, stdout, stderr) {
-      if (error !== null) {
-        log.error("[HOME] PlaySound Error: " + error.toString());
+  function initGoogleVoice() {
+    gv = new GoogleVoice(config.gvoice.interval);
+    gv.on("changed", function(changeType, data) {
+      if (changeType === "new") {
+        if (_self.state.system_state === "HOME") {
+          _self.set("GV_NEW");
+        }
+      } else if (changeType === "zero") {
+        if (_self.state.system_state === "HOME") {
+          _self.set("GV_ZERO");
+        }
+      }
+      _self.state.gvoice = data;
+      fbSet("state/gvoice", data);
+    });
+    gv.on("error", function(error) {
+      if (_self.state.system_state === "HOME") {
+        // TODO: add better error logging here
+        _self.set("GV_ERROR");
+        _self.state.gvoice = error;
+        fbSet("state/gvoice", error);
       }
     });
-    log.debug("PlaySound: " + file);
-  }, 1);
-};
-
-function initInsideTemp(self) {
-  var insideTemp = new InsideTemperature(self.config.temperature.inside.interval);
-  insideTemp.on("error", function(error) {
-    log.error("[HOME] Error reading inside temperature: " + error);
-    self.state.temperature.inside = error;
-    fbSet("state/temperature/inside", error);
-  });
-  insideTemp.on("update", function(data) {
-    self.state.temperature.inside = data.f;
-    fbSet("state/temperature/inside", data.f);
-    log.debug("[HOME] Inside temperature is " + data.f + "F");
-  });
-}
-
-function initOutsideTemp(self) {
-  var url = "https://publicdata-weather.firebaseio.com/";
-  url += self.config.temperature.outside.city + "/currently";
-  var weatherRef = new Firebase(url);
-  weatherRef.child('temperature').on('value', function(snapshot) {
-    self.state.temperature.outside = snapshot.val();
-    fbSet("state/temperature/outside", snapshot.val());
-    log.debug("[HOME] Outside temperature is " + snapshot.val() + "F");
-  });
-}
-
-function initAC(self) {
-  airConditioners = {};
-  var ip = self.config.airconditioners.itach_ip;
-  var port = self.config.airconditioners.itach_port;
-  self.config.airconditioners.ac.forEach(function(elem, idx, arr) {
-    var id = elem.id;
-    var cmds = self.config.airconditioners.commands[elem.protocol];
-    airConditioners[id] = new AirConditioner(id, ip, port, cmds);
-    self.state.ac[id] = 0;
-    fbSet("state/ac/" + id, 0);
-  });
-}
-
-function initHarmony(self) {
-  harmony = new Harmony();
-  harmony.on("changed", function(activity) {
-    self.state.harmony_activity = activity;
-    fbSet("staet/harmony_activity", activity);
-    log.debug("[HOME] Harmony activity changed: " + activity);
-  });
-  harmony.getConfig(function(cfg) {
-    self.config.harmony = cfg;
-    fbSet("harmony", cfg);
-  });
-}
-
-function initHue(self) {
-  hue = new Hue(self.config.hue.interval, Keys.keys.hue, self.config.hue.ip);
-  hue.on("update", function(data) {
-    self.state.hue = data;
-    fbSet("state/hue", data);
-  });
-  hue.on("error", function (err) {
-    var error = {"error": true, "result": err};
-    self.state.hue = error;
-    fbSet("state/hue", error);
-    log.error("[HOME] Error reading Hue state.");
-    log.debug(JSON.stringify(err));
-  });
-}
-
-function initDoor(self) {
-  self.state.door = false;
-  var door = new Door();
-  door.on("no-gpio", function() {
-    self.state.door = undefined;
-    fbSet("state/door", null);
-    log.error("[HOME] No GPIO found for door detection.");
-  });
-  door.on("changed", function(data) {
-    if (self.state.system_state === "AWAY") {
-      self.set("HOME");
-    }
-    self.state.door = data;
-    fbSet("state/door", data);
-    fbPush("door", {"date": Date.now(), "state": data});
-  });
-}
-
-function initGoogleVoice(self) {
-  var gv = new GoogleVoice(self.config.gvoice.interval);
-  gv.on("changed", function(changeType, data) {
-    if (changeType === "new") {
-      if (self.state.system_state === "HOME") {
-        self.set("GV_NEW");
-      }
-    } else if (changeType === "zero") {
-      if (self.state.system_state === "HOME") {
-        self.set("GV_ZERO");
-      }
-    }
-    self.state.gvoice = data;
-    fbSet("state/gvoice", data);
-  });
-  gv.on("error", function(error) {
-    if (self.state.system_state === "HOME") {
-      self.set("GV_ERROR");
-      self.state.gvoice = error;
-      fbSet("state/gvoice", error);
-    }
-  });
-}
-
-function initFirebase(self) {
-  fb = new Firebase("https://boiling-torch-4633.firebaseio.com/");
-  fb.auth(Keys.keys.fb, function(error) {
-    if(error) {
-      
-    } else {
-      
-    }
-  });
-  fbSet("config", self.config);
-  fbPush("system", {"date": Date.now(), "action": "started"});
-  fb.child("state").remove();
-  fbSet("state", self.state);
-}
-
-function onReady(self) {
-  if (self.config) {
-    self.state.system_state = "AWAY";
-    initFirebase(self);
-    initAC(self);
-    initGoogleVoice(self);
-    initInsideTemp(self);
-    initOutsideTemp(self);
-    initDoor(self);
-    initHarmony(self);
-    initHue(self);
-    self.emit("ready");
-    playSound(self.config.ready_sound);
   }
+
+  function loadConfig() {
+    log.log("[HOME] Reading config from Firebase.");
+    fb.child("config").on("value", function(snapshot) {
+      config = snapshot.val();
+      log.log("[HOME] Config file updated.");
+      if (ready === false) {
+        init();
+      }
+    });
+  }
+
+  function init() {
+    ready = true;
+    log.log("[HOME] Initalizing components.");
+    _self.state.system_state = "AWAY";
+    initAC();
+    initGoogleVoice();
+    initInsideTemp();
+    initOutsideTemp();
+    initDoor();
+    initHarmony();
+    initHue();
+    _self.emit("ready");
+    playSound(config.ready_sound);
+  }
+
+  loadConfig();
 }
+
+
+util.inherits(Home, EventEmitter);
 
 module.exports = Home;
