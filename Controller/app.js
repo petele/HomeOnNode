cc80f160ff4c6a7536991a16cf8f7e723cbc6d6b
Pete LePage <pete.lepage@pobox.com>
2014-08-26 14:55:48 -0400

    updated app

--- Controller/app.js
+++ Controller/app.js
@@ -2,8 +2,27 @@ var log = require("./SystemLog");
 var Home = require("./Home");
 var Keys = require("./Keys");
 var HTTPServer = require("./HTTPServer");
+var Firebase = require("firebase");
 
 
-var home = new Home();
+var fb, home, httpServer;
 
-var httpServer = new HTTPServer(home);
+function init() {
+  log.appStart();
+  fb = new Firebase("https://boiling-torch-4633.firebaseio.com/");
+  fb.auth(Keys.keys.fb, function(error) {
+    if(error) {
+      
+    } else {
+      home = new Home(fb);
+      home.on("ready", function() {
+        httpServer = new HTTPServer(home);
+        // fb.child("commands").on("child_added", function(snapshot) {
+        //   console.log("ZZZZZ", snapshot.val());
+        // });
+      });
+    }
+  });
+}
+
+init();
