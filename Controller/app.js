3085dec917362b2f96b5224cf4544e185a0e3466
Pete LePage <pete.lepage@pobox.com>
2014-08-27 09:54:58 -0400

    read config file locally at start up

--- Controller/app.js
+++ Controller/app.js
@@ -3,28 +3,31 @@ var Home = require("./Home");
 var Keys = require("./Keys");
 var HTTPServer = require("./HTTPServer");
 var Firebase = require("firebase");
+var fs = require("fs");
 
 
 var fb, home, httpServer;
 
 function init() {
-  log.appStart();
+  log.appStart("HomeOnNode");
   fb = new Firebase("https://boiling-torch-4633.firebaseio.com/");
   fb.auth(Keys.keys.fb, function(error) {
     if(error) {
       
     } else {
-      home = new Home(fb);
-      home.on("ready", function() {
-        httpServer = new HTTPServer(home);
-        fb.child("commands").on("child_added", function(snapshot) {
-          try {
-            var cmd = snapshot.val();
-            home.set(cmd.command, cmd.modifier, "FB");
-            snapshot.ref().remove();
-          } catch (ex) {
-            log.error("Unable to execute FireBase Command: " + JSON.stringify(cmd));
-          }
+      fs.readFile("./config.json", {"encoding": "utf8"}, function(data) {
+        home = new Home(data, fb);
+        home.on("ready", function() {
+          httpServer = new HTTPServer(home);
+          fb.child("commands").on("child_added", function(snapshot) {
+            try {
+              var cmd = snapshot.val();
+              home.set(cmd.command, cmd.modifier, "FB");
+              snapshot.ref().remove();
+            } catch (ex) {
+              log.error("Unable to execute FireBase Command: " + JSON.stringify(cmd));
+            }
+          });
         });
       });
     }
