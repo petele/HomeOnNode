5c69a653a5085c0f3fe292f4e0b6ec624331d3b8
Pete LePage <pete.lepage@pobox.com>
2014-08-27 09:29:27 -0400

    Updated helper apps

--- Door/app.js
+++ Door/app.js
@@ -1,59 +1,45 @@
 var fs = require("fs");
-var http = require("http");
 var Door = require("./Door");
+var sendCommand = require("./sendCommand");
+var log = require("./SystemLog");
 
 var config;
+var modifier;
 
-function sendCommand(body) {
-  var options = {
-    hostname: config.ip,
-    port: config.port,
-    path: "/state",
-    method: "POST"
-  };
-  var request = http.request(options, function(response) {
-    var result = "";
-    response.setEncoding('utf8');
-    response.on('data', function (chunk) {
-      result += chunk;
-    });
-    response.on("end", function() {
-      console.log(body, result);
-    });
-  });
-  request.on("error", function(error) {
-    console.error(body, error.code);
-  });
-  request.setTimeout(2500, function() {
-    request.abort();
-  });
+log.appStart("Door Listener");
 
-  request.write(JSON.stringify(body));
-
-  request.end();
-
-}
 
 function listen() {
   var door = new Door();
   door.on("no-gpio", function() {
-    console.log("ERROR: No GPIO found.");
+    log.error("No GPIO found.");
   });
   door.on("changed", function(data) {
     if (data === true) {
-      sendCommand(config.onOpen);
+      log.level("DOOR", "OPENED");
+      sendCommand.send(config.onOpen.command, config.onOpen.modifier);
     } else {
-      sendCommand(config.onClose);
+      log.level("DOOR", "CLOSED");
+      sendCommand.send(config.onClose.command, config.onClose.modifier);
     }
   });
 }
 
+function exit() {
+  log.appStop("SIGINT");
+  process.exit();
+}
+
+process.on('SIGINT', exit);
+
 
 fs.readFile("config.json", {"encoding": "utf8"}, function(err, data) {
   if (err) {
-    console.log("ERROR: Unable to open config file.");
+    log.error("Unable to open config file.");
   } else {
     config = JSON.parse(data);
+    sendCommand.setConfig(config);
+    log.log("Ready.");
     listen();
   }
 });
--- Keypad/app.js
+++ Keypad/app.js
@@ -1,48 +1,13 @@
 var keypress = require("keypress");
 var fs = require("fs");
-var http = require("http");
+var sendCommand = require("./sendCommand");
+var log = require("./SystemLog");
+
 
 var config;
 var modifier;
 
-function sendCommand(command, modifier) {
-  var body = JSON.stringify({
-    "command": command,
-    "modifier": modifier
-  });
-  var options = {
-    hostname: config.ip,
-    port: config.port,
-    path: "/state",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": body.length
-    }
-  };
-
-  var request = http.request(options, function(response) {
-    var result = "";
-    response.setEncoding('utf8');
-    response.on('data', function (chunk) {
-      result += chunk;
-    });
-    response.on("end", function() {
-      console.log(body, result);
-    });
-  });
-  request.on("error", function(error) {
-    console.error(body, error.code);
-  });
-  request.setTimeout(2500, function() {
-    request.abort();
-  });
-
-  request.write(body);
-
-  request.end();
-
-}
+log.appStart("KeyPad");
 
 function listen() {
   // make `process.stdin` begin emitting "keypress" events
@@ -66,11 +31,12 @@ function listen() {
         modifier = undefined;
       }, 5000);
     } else if (k) {
-      sendCommand(k, modifier);
+      sendCommand.send(k, modifier);
       modifier = undefined;
     }
     
     if (key && key.ctrl && key.name === 'c') {
+      log.appStop("SIGINT");
       process.stdin.pause();
     }
   });
@@ -81,12 +47,13 @@ function listen() {
 
 
 
-
 fs.readFile("config.json", {"encoding": "utf8"}, function(err, data) {
   if (err) {
-    console.log("ERROR: Unable to open config file.")
+    log.error("Unable to open config file.");
   } else {
     config = JSON.parse(data);
+    sendCommand.setConfig(config);
+    log.log("Ready.");
     listen();
   }
 });
