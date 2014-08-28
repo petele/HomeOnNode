aa322629f92f3f9a9daaf8324419f36d3d31953e
Pete LePage <pete.lepage@pobox.com>
2014-08-27 22:59:04 -0400

    door

--- Controller/Door.js
+++ Controller/Door.js
@@ -1,35 +1,31 @@
 var EventEmitter = require("events").EventEmitter;
 var util = require("util");
-var Gpio = require("onoff").Gpio;
+//var Gpio = require("onoff").Gpio;
 var log = require("./SystemLog");
 
 function Door(label, pin_num) {
+  var Gpio = require("onoff").Gpio,
+      pin = new Gpio(pin_num, in);
   this.label = label;
   this.state = "UNKNOWN";
   var self = this;
 
-  try {
-    var pin = new Gpio(pin_num, "in");
 
-    pin.watch(function(err, value) {
-      log.debug("PIN HERE " + value.toString());
-      if (err) {
-        log.error("PIN CHANGE ERROR " + err);
-        self.emit("error", err);
+  pin.watch(function(err, value) {
+    log.debug("PIN HERE " + value.toString());
+    if (err) {
+      log.error("PIN CHANGE ERROR " + err);
+      self.emit("error", err);
+    } else {
+      log.debug("PIN CHANGE " + value.toString());
+      if (value === 1) {
+        self.state = "OPEN";
       } else {
-        log.debug("PIN CHANGE " + value.toString());
-        if (value === 1) {
-          self.state = "OPEN";
-        } else {
-          self.state = "CLOSED";
-        }
-        self.emit("change", self.state);
+        self.state = "CLOSED";
       }
-    });
-  } catch (ex) {
-    log.error("DOOR EXCEPTION " + ex.toString());
-    self.emit("error", ex);
-  }
+      self.emit("change", self.state);
+    }
+  });
   log.init("[Door] " + label + " Pin: " + pin_num);
 }
 
