e7f31d85df2fd8491f35b9a9ba47f06e56cefa63
Pete LePage <pete.lepage@pobox.com>
2014-08-28 09:49:17 -0400

    temp disabled door functionality

--- Controller/Door.js
+++ Controller/Door.js
@@ -1,6 +1,5 @@
 var EventEmitter = require("events").EventEmitter;
 var util = require("util");
-var gpio = require("onoff").Gpio;
 var log = require("./SystemLog");
 
 
@@ -9,22 +8,23 @@ function Door(label, pin_num) {
   this.state = "UNKNOWN";
   var self = this;
 
-  try {
-    var pin = new Gpio(pin_num, "in");
+  // try {
+  //   var Gpio = require("onoff").Gpio;
+  //   var pin = new Gpio(pin_num, "in");
 
-    pin.watch(function(err, val) {
-      log.debug("PIN WATCH" + val.toString());
-      if (val === 1) {
-        self.state = "OPEN";
-      } else {
-        self.state = "CLOSED";
-      }
-      self.emit("change", self.state);
-    });
-  } except (ex) {
-    self.emit("error", ex);
-  }
-  log.init("[Door] " + label + " Pin: " + pin);
+  //   pin.watch(function(err, val) {
+  //     log.debug("PIN WATCH" + val.toString());
+  //     if (val === 1) {
+  //       self.state = "OPEN";
+  //     } else {
+  //       self.state = "CLOSED";
+  //     }
+  //     self.emit("change", self.state);
+  //   });
+  // } catch (ex) {
+  //   self.emit("error", ex);
+  // }
+  log.init("[Door] " + label + " Pin: " + pin_num);
 }
 
 util.inherits(Door, EventEmitter);
