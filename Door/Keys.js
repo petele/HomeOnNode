1561f72eb2dd5f3602efe4fe5205c36bcd14017c
Pete LePage <pete.lepage@pobox.com>
2014-08-28 16:17:32 -0400

    Door tool

---
+++ Door/Keys.js
@@ -0,0 +1,1 @@
+../Controller/Keys.js
--- Door/app.js
+++ Door/app.js
@@ -2,44 +2,51 @@ var fs = require("fs");
 var Door = require("./Door");
 var sendCommand = require("./sendCommand");
 var log = require("./SystemLog");
+var fbHelper = require("./fbHelper");
+var Keys = require("./Keys");
 
 var config;
-var modifier;
+var fb;
 
 log.appStart("Door Listener");
 
-
 function listen() {
-  var door = new Door();
+  var door = new Door(config.id, config.pin);
   door.on("no-gpio", function() {
     log.error("No GPIO found.");
+    //exit("NO-GPIO", 1);
   });
   door.on("changed", function(data) {
-    if (data === true) {
-      log.level("DOOR", "OPENED");
+    if (data === "OPEN") {
       sendCommand.send(config.onOpen.command, config.onOpen.modifier);
     } else {
-      log.level("DOOR", "CLOSED");
       sendCommand.send(config.onClose.command, config.onClose.modifier);
     }
   });
 }
-
-function exit() {
-  log.appStop("SIGINT");
-  process.exit();
-}
-
-process.on('SIGINT', exit);
-
 
 fs.readFile("config.json", {"encoding": "utf8"}, function(err, data) {
   if (err) {
     log.error("Unable to open config file.");
   } else {
     config = JSON.parse(data);
+    fb = fbHelper.init(Keys.keys.fb, "door-" + config.id, exit);
     sendCommand.setConfig(config);
     log.log("Ready.");
     listen();
   }
+});
+
+function exit(sender, exitCode) {
+  exitCode = exitCode || 0;
+  log.log("[APP] Starting shutdown process");
+  log.log("[APP] Will exit with error code: " + String(exitCode));
+  setTimeout(function() {
+    log.appStop(sender);
+    process.exit(exitCode);
+  }, 1500);
+}
+
+process.on('SIGINT', function() {
+  exit("SIGINT", 0);
 });
