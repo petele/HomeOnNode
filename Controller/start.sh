23b74c81b2d83c139e4daa4635fd376e10fd3887
Pete LePage <pete.lepage@pobox.com>
2014-08-29 11:19:16 -0400

    script to start controller and reboot as necessary

---
+++ Controller/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+check_exit_code() {
+  if [ "${1}" -eq "0" ]; then
+    echo "Normal exit."
+  elif [ "${1}" -eq "10" ]; then
+    echo "Reboot requested."
+    sudo reboot
+  else
+    echo "Unknown exit code: ${1}"
+    echo "${1}" >> exitcodes.txt
+  fi  
+}
+
+git reset --hard
+npm install
+sudo node app.js
+
+# node temp.js
+
+check_exit_code $?
+
+
