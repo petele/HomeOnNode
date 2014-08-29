cc02d2fac2d63d3d5c67f28bd7001b1aef4ee1db
Pete LePage <pete.lepage@pobox.com>
2014-08-29 18:25:51 -0400

    better start up

--- Controller/start.sh
+++ Controller/start.sh
@@ -12,11 +12,24 @@ check_exit_code() {
   fi
 }
 
+echo Setting Up Thermometer...
+sudo modprobe w1-gpio
+sudo modprobe w1-therm
+
+echo Resetting any local file changes...
 git reset --hard
+
+echo Getting latest version...
 git pull
+
+echo Getting latest keyboard commands...
 rm ./keypad.json
 cp ../KeypadConfigs/primary.json ./keypad.json
+
+echo Updating any node modules...
 npm install
+
+echo Starting app...
 sudo node app.js
 
 # node temp.js
