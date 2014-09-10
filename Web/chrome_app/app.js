31c01ac6f2b0e548a430b2fc107d3b020e8d652f
Pete LePage <pete.lepage@pobox.com>
2014-09-09 21:48:31 -0400

    previous UI

---
+++ Web/chrome_app/app.js
@@ -0,0 +1,17 @@
+
+var wv = document.getElementById("wvMain");
+// wv.addEventListener("sizechanged", wvSizeChanged);
+
+// function wvSizeChanged(oldW, oldH, newW, newH) {
+//   console.log("wvSizeChanged", oldW, oldH, newW, newH);
+// }
+
+/*
+chrome.app.window.current().onBoundsChanged.addListener(updateCurrentStateReadout);
+
+function updateCurrentStateReadout(a,b,c) {
+  console.log("AAA", a, b, c);
+  var value = (chrome.app.window.current().getBounds().width - 25) + "px";
+  wv.style.width = value;
+}
+*/
