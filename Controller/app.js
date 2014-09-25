035141031f5865e66640120a1a0e35ec104f6674
Pete LePage <pete.lepage@pobox.com>
2014-09-25 17:44:36 -0400

    removed unused function

--- Controller/app.js
+++ Controller/app.js
@@ -138,13 +138,5 @@ process.on('SIGINT', function() {
   exit("SIGINT", 0);
 });
 
-// process.on("uncaughtException", function(err) {
-//   log.error("**** UNCAUGHT EXCEPTION ****");
-//   log.error(err);
-//   log.error("Exiting with error code 2.");
-//   exit("UNCAUGHT_EXCEPTION", 2);
-// });
-
-
 
 init();
