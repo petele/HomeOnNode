09fc6f1e0fb7abecbc8f210e54863aa58c532503
Pete LePage <pete.lepage@pobox.com>
2014-09-30 20:56:17 -0400

    modifying start up

--- Keypad/start.sh
+++ Keypad/start.sh
@@ -12,16 +12,6 @@ check_exit_code() {
   fi
 }
 
-echo Resetting any local file changes...
-git reset --hard
-
-echo Getting latest version...
-git pull
-
-echo Creating lastest version file...
-last_commit=$(git rev-list HEAD --max-count=1 | cut -c1-7)
-sed "s/\[HEAD\]/$last_commit/g" ../Controller/version.js > v.js && mv v.js ../Controller/version.js
-
 echo Getting latest keyboard commands...
 rm ./keypad.json
 cp ../KeypadConfigs/bedside.json ./keypad.json
