b39f2f5c6394aa7ccece07f3c7fb3cfdd8f9a048
Pete LePage <pete.lepage@pobox.com>
2014-10-16 18:15:31 -0400

    better error handling for chrome cast errors

--- Controller/Chromecast.js
+++ Controller/Chromecast.js
@@ -14,58 +14,89 @@ function Chromecast(ip) {
       this.stopApp();
     }
 
-    client = new Client();
-    client.connect(ip, function() {
+    try {
+      client = new Client();
+      client.connect(ip, function() {
 
-      connected = true;
+        // create various namespace handlers
+        connection = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.connection', 'JSON');
+        heartbeat  = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.heartbeat', 'JSON');
+        receiver   = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.receiver', 'JSON');
 
-      // create various namespace handlers
-      connection = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.connection', 'JSON');
-      heartbeat  = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.tp.heartbeat', 'JSON');
-      receiver   = client.createChannel('sender-0', 'receiver-0', 'urn:x-cast:com.google.cast.receiver', 'JSON');
+        // establish virtual connection to the receiver
+        connection.send({ type: "CONNECT" });
 
-      // establish virtual connection to the receiver
-      connection.send({ type: "CONNECT" });
+        // start heartbeating
+        heartbeatId = setInterval(function() {
+          try {
+            heartbeat.send({ type: "PING" });
+          } catch (ex) {
+            log.exception("[ChromeCast] Unable to send heartbeat ping.", ex);
+          }
+        }, 5000);
 
-      // start heartbeating
-      heartbeatId = setInterval(function() {
-        heartbeat.send({ type: "PING" });
-      }, 5000);
+        // launch the specified app
+        receiver.send({ type: "LAUNCH", appId: appId, requestId:  requestId});
+        requestId++;
 
-      // launch the specified app
-      receiver.send({ type: "LAUNCH", appId: appId, requestId:  requestId});
-      requestId++;
+        // display receiver status updates
+        receiver.on("message", function(data, broadcast) {
+          log.debug("[ChromeCast Receiver] " + JSON.stringify(data));
+        });
 
-      // display receiver status updates
-      receiver.on("message", function(data, broadcast) {
-        log.debug("[ChromeCast Receiver] " + JSON.stringify(data));
+        connected = true;
+
+        log.log("[ChromeCast] Started App [" + appId + "]");
       });
-
-      log.log("[ChromeCast] Started App [" + appId + "]");
-    });
+    } catch (ex) {
+      this.stopApp();
+      log.exception("[ChromeCast] Exception when starting app [" + appId + "]", ex);
+    }
+    return connected;
   };
 
   this.stopApp = function() {
-    if (heartbeatId) {
-      clearInterval(heartbeatId);
-      heartbeatId = null;
-      heartbeat = null;
-      log.debug("[ChromeCast] Heartbeat stopped.");
+    var success = true;
+
+    try {
+      if (heartbeatId) {
+        clearInterval(heartbeatId);
+        heartbeatId = null;
+        heartbeat = null;
+        log.debug("[ChromeCast] Heartbeat stopped.");
+      }
+    } catch (ex) {
+      success = false;
+      log.exception("[ChromeCast] Error stopping heartbeat.", ex);
     }
-    if (receiver) {
-      receiver.send({type: "STOP", requestId: requestId});
-      requestId++;
-      receiver = null;
-      log.debug("[ChromeCast] Receiver stopped.");
+
+    try {
+      if (receiver) {
+        receiver.send({type: "STOP", requestId: requestId});
+        requestId++;
+        receiver = null;
+        log.debug("[ChromeCast] Receiver stopped.");
+      }
+    } catch (ex) {
+      success = false;
+      log.exception("[ChromeCast] Error stopping receiver.", ex);
     }
-    if (connection) {
-      connection.send({type: "CLOSE", requestId: requestId});
-      requestId++;
-      connection = null;
-      log.debug("[ChromeCast] Connection closed.");
+
+    try {
+      if (connection) {
+        connection.send({type: "CLOSE", requestId: requestId});
+        requestId++;
+        connection = null;
+        log.debug("[ChromeCast] Connection closed.");
+      }
+    } catch (ex) {
+      success = false;
+      log.exception("[ChromeCast] Error closing connection.", ex);
     }
+
     connected = false;
     log.log("[ChromeCast] App Stopped.");
+    return success;
   };
 
   log.init("[ChromeCast]");
