9703a3415fdb477eaecc37ba21c5728a12f33f3e
Pete LePage <pete.lepage@pobox.com>
2015-03-04 18:39:43 -0500

    test harness

--- Controller/test.js
+++ Controller/test.js
@@ -1,17 +1,42 @@
 var Presence = require("./Presence");
 var log = require("./SystemLog");
+var webRequest = require("./webRequest");
 
 var people = [
-      {"name": "Pete", "uuid": "88d89697672b4a429060e8a1d75236c4"},
-      {"name": "FitBit", "uuid": "xyz789"}
+      {"name": "Pete (Mac)", "uuid": "9660b3843c5648299827400de1251b06"},
+      {"name": "Pete (Linux)", "uuid": "c05391b2f3fc"}
     ];
 var presence = new Presence(people);
 
 
 presence.on("change", function(data) {
   log.log("[TestHarness] " + data.present);
-  //console.log("change", data);
+  if (data.present === 0) {
+    send("Off");
+  } else {
+    send();
+  }
 });
+
+
+function send(modifier) {
+  var uri = {
+    "host": "192.168.1.202",
+    "port": 3000,
+    "path": "/state",
+    "method": "POST"
+  };
+  var body = {
+    "command": "ALERT_TEST",
+    "modifier": modifier
+  };
+  body = JSON.stringify(body);
+  log.http("REQ", body);
+  webRequest.request(uri, body, function(resp) {
+    log.http("RESP", JSON.stringify(resp));
+  });
+}
+
 
 // ----
 
