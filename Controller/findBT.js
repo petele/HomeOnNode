ae2d3a1929d1585e19a4f1533696daeeb7f20d4c
Pete LePage <pete.lepage@pobox.com>
2015-03-04 18:21:30 -0500

    findBT clean up

--- Controller/findBT.js
+++ Controller/findBT.js
@@ -1,5 +1,17 @@
 var noble = require('noble');
+var ansi = require('ansi'), cursor = ansi(process.stdout);
 
+var devices = [];
+
+var numLines = process.stdout.rows;
+function lf () { return '\n'; }
+var pad = "                                             ";
+
+cursor.write(Array.apply(null, Array(process.stdout.getWindowSize()[1])).map(lf).join(''))
+  .eraseData(2)
+  .goto(1, 1)
+console.log("UUID                              Name                RSSI TXPr");
+console.log("--------------------------------- ------------------- ---- ----");
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
@@ -9,10 +21,12 @@ noble.on('stateChange', function(state) {
   }
 });
 
-console.log("UUID                              Name                RSSI TXPr");
-console.log("--------------------------------- ------------------- ---- ----");
 noble.on('discover', function(peripheral) {
-  var pad = "                                             ";
+  var index = devices.indexOf(peripheral.uuid);
+  if (index === -1) {
+    devices.push(peripheral.uuid);
+    index = devices.length - 1;
+  }
   var uuid = (pad + peripheral.uuid).slice(-32);
   var localName = peripheral.advertisement.localName;
   if (localName === undefined) { localName = ""; }
@@ -21,5 +35,13 @@ noble.on('discover', function(peripheral) {
   var txPower = peripheral.advertisement.txPowerLevel;
   if (txPower === undefined) { txPower = "0000"; }
   txPower = (pad + txPower).slice(-4);
+  cursor.goto(1, index+3);
   console.log(uuid, localName, rssi, txPower);
+});
+
+process.on('SIGINT', function() {
+  var lines = devices.length;
+  cursor.goto(1, lines+4);
+  cursor.reset();
+  process.exit();
 });
