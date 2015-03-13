68bdebe476381a4d71a625e6198c05c70715656e
Pete LePage <pete.lepage@pobox.com>
2015-03-13 12:28:12 -0400

    added error toast

--- Web/scripts/main.js
+++ Web/scripts/main.js
@@ -1,26 +1,48 @@
-
 window.lastEvent = Date.now();
 
 var pToast = document.querySelector("paper-toast");
+var pErrorToast = document.querySelector("error-toast");
+var ignoreError = true;
 
 var fb = new Firebase("https://boiling-torch-4633.firebaseio.com/");
 fb.authWithCustomToken(fbKey, function(error) {
   if(error) {
     console.error("[FIREBASE] Auth failed. " + error.toString());
-    showToast("Firebase authentication failure.");
+    window.showErrorToast("Firebase authentication failure.");
   } else {
     console.log("[FIREBASE] Auth success.");
     fb.child(".info/connected").on("value", function(snapshot) {
       if (snapshot.val() === true) {
         window.showToast("Firebase connected.");
       } else {
-        window.showToast("Network disconnected.");
+        window.showErrorToast("Network disconnected.");
       }
+    });
+    fb.child("logs/errors").limitToLast(1).on("child_added", function(snapshot) {
+      if (ignoreError === true) {
+        ignoreError = false;
+      } else {
+        var err = snapshot.val();
+        window.showErrorToast(err.message);
+      }
+
     });
   }
 });
 
-
+window.showErrorToast = function(message) {
+  if (pToast.opened === true) {
+    pToast.dismiss();
+  }
+  if (message.length > 60) {
+    message = message.substring(0, 59) + "...";
+  }
+  pErrorToast.text = message;
+  pErrorToast.show();
+  if (navigator.vibrate) {
+    navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]);
+  }
+};
 
 window.showToast = function(message) {
   pToast.text = message;
