2051a8123a2604b611829bbc6d43a7205ad428d3
Pete LePage <pete.lepage@pobox.com>
2015-04-20 21:54:37 -0400

    start up stuff

--- closet.sh
+++ closet.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+echo Getting latest keyboard commands...
+rm ./Remote/config.json
+cp ./Configs/closet.json ./Remote/config.json
 
 echo Exporting pin 23 and pulling up
 gpio-admin export 23 pullup
+
+cd Remote
+
+echo Updating any node modules...
+npm install
 
 echo Starting app...
 OUTPUT=$(node app.js 2>&1 >/dev/tty)
