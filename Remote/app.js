3873b8121a317ad2a578a4340787d98a7abeeda8
Pete LePage <pete.lepage@pobox.com>
2015-04-22 17:51:43 -0400

    jshint

--- Remote/app.js
+++ Remote/app.js
@@ -1,54 +1,56 @@
-var fs = require("fs");
-var log = require("../Controller/SystemLog");
-var fbHelper = require("../Controller/fbHelper");
-var Keys = require("../Controller/Keys");
-var webRequest = require("../Controller/webRequest");
+'use strict';
 
-var Door = require("../Controller/Door");
-var Keypad = require("../Controller/Keypad");
-//var Dimmer = require("./Dimmer");
+var fs = require('fs');
+var log = require('../Controller/SystemLog');
+var fbHelper = require('../Controller/fbHelper');
+var Keys = require('../Controller/Keys');
+var webRequest = require('../Controller/webRequest');
 
-var fb, config, keypad, door, dimmer;
+var Door = require('../Controller/Door');
+var Keypad = require('../Controller/Keypad');
+//var Dimmer = require('./Dimmer');
 
-log.appStart("Remote");
+var fb, config, door;
+
+log.appStart('Remote');
 
 function sendCommand(command) {
   var uri = {
-    "host": config.ip,
-    "port": config.port,
-    "path": "/state",
-    "method": "POST"
+    'host': config.ip,
+    'port': config.port,
+    'path': '/state',
+    'method': 'POST'
   };
-  if (typeof command === "object") {
+  if (typeof command === 'object') {
     command = JSON.stringify(command);
   }
   try {
-    log.http("REQ", command);
+    log.http('REQ', command);
     webRequest.request(uri, command, function(resp) {
-      log.http("RESP", JSON.stringify(resp));
+      log.http('RESP', JSON.stringify(resp));
     });
   } catch (ex) {
-    log.error("[sendCommand] " + ex.toString());
+    log.error('[sendCommand] ' + ex.toString());
   }
 }
 
 
-fs.readFile("config.json", {"encoding": "utf8"}, function(err, data) {
+fs.readFile('config.json', {'encoding': 'utf8'}, function(err, data) {
   if (err) {
-    log.error("Unable to open config file.");
+    log.error('Unable to open config file.');
   } else {
     config = JSON.parse(data);
-    fb = fbHelper.init(Keys.keys.fb, "remote-" + config.id, exit);
+    fb = fbHelper.init(Keys.keys.fb, 'remote-' + config.id, exit);
 
     // Check door state and start monitoring door
     if ((config.door) && (config.door.enabled === true)) {
       door = new Door(config.id, config.door.pin);
-      door.on("no-gpio", function(e) {
-        log.error("[DOOR] No GPIO for door " + config.id + " " + e.toString());
+      door.on('no-gpio', function(e) {
+        log.error('[DOOR] No GPIO for door ' + config.id + ' ' + e.toString());
       });
-      door.on("change", function(data) {
-        log.log("[DOOR] " + config.id + " " + data);
-        if (data === "OPEN") {
+      door.on('change', function(data) {
+        log.log('[DOOR] ' + config.id + ' ' + data);
+        if (data === 'OPEN') {
           sendCommand(config.door.onOpen);
         } else {
           sendCommand(config.door.onClose);
@@ -74,8 +76,8 @@ fs.readFile("config.json", {"encoding": "utf8"}, function(err, data) {
 
 function exit(sender, exitCode) {
   exitCode = exitCode || 0;
-  log.log("[APP] Starting shutdown process");
-  log.log("[APP] Will exit with error code: " + String(exitCode));
+  log.log('[APP] Starting shutdown process');
+  log.log('[APP] Will exit with error code: ' + String(exitCode));
   setTimeout(function() {
     log.appStop(sender);
     process.exit(exitCode);
@@ -83,5 +85,5 @@ function exit(sender, exitCode) {
 }
 
 process.on('SIGINT', function() {
-  exit("SIGINT", 0);
+  exit('SIGINT', 0);
 });
