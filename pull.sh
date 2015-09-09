a01147bd23f77cb893f4cfe2bd7ce2fbb7c0dccb
Pete LePage <pete.lepage@pobox.com>
2015-09-08 21:27:47 -0400

    do not update node modules

--- pull.sh
+++ pull.sh
@@ -10,7 +10,8 @@ echo "Creating lastest version file..."
 last_commit=$(git rev-list HEAD --max-count=1 | cut -c1-7)
 sed "s/\[HEAD\]/$last_commit/g" ./app/version.js > v.js && mv v.js ./app/version.js
 
-echo "Updating any node modules..."
-cd app
-npm install
-cd ..
+# Do not update node modules
+# echo "Updating any node modules..."
+# cd app
+# npm install
+# cd ..
