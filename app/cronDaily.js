9e2f9a8fe58618daf94c52f641bb4f7bcef75161
Pete LePage <pete.lepage@pobox.com>
2015-12-17 09:23:24 -0500

    tighter log clean up

--- app/cronDaily.js
+++ app/cronDaily.js
@@ -4,10 +4,10 @@
 
 var cronJob = function() {
   log.log('[CronDaily]');
-  log.cleanLogs('logs/doors');
+  log.cleanLogs('logs/doors', 30);
   log.cleanLogs('logs/logs', 4);
   log.cleanLogs('logs/presence');
-  log.cleanLogs('logs/systemState');
+  log.cleanLogs('logs/systemState', 30);
 };
 
 cronJob();
