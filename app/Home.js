e85b0263b85382cffd6b9850597448eed8fed444
Pete LePage <pete.lepage@pobox.com>
2016-01-19 22:33:20 -0500

    auto mode set

--- app/Home.js
+++ app/Home.js
@@ -744,12 +744,14 @@ function Home(config, fb) {
       sonos.on('transport-state', function(transportState) {
         fbSet('state/sonos/state', transportState);
         try {
-          if ((_self.state.harmony.id === -1) && 
+          if ((_self.state.harmony) && 
+              (_self.state.harmony.id === '-1') &&
               (transportState.state.zoneState === 'PLAYING')) {
             setHarmonyActivity('Sonos');
           }
         } catch (ex) {
-          log.warn('[HOME] Unable to update zone state.');
+          log.exception('[HOME] Unable to update zone state.', ex);
+          console.log('xxx', ex);
         }
       });
     }
