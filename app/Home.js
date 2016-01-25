099c48d89b445599fe24555d6f63bcd9e58acadc
Pete LePage <pete.lepage@pobox.com>
2016-01-25 12:14:06 -0500

    more logging

--- app/Home.js
+++ app/Home.js
@@ -144,7 +144,7 @@ function Home(config, fb) {
         cmds = [cmds];
       }
       cmds.forEach(function(cmd) {
-        log.debug('XXXX', JSON.stringify(cmd));
+        log.debug('XXXX - ' + JSON.stringify(cmd));
         if (modifier) {
           adjustNestThermostat(cmd.roomId, modifier);
         } else {
