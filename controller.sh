ed9f3c9734f6aa24a8d4aaedcaa50de9a937b707
Pete LePage <pete.lepage@pobox.com>
2016-02-24 18:22:20 -0500

    Moved forever to login

--- controller.sh
+++ controller.sh
@@ -3,10 +3,6 @@
 cd app
 
 echo ""
-echo "Starting Forever in the background..."
-forever start -l logs/forever.log -o logs/forever-out.log -e logs/forever-err.log appMonitor.js
-
-echo ""
 node getConfig.js HomeOnNode
 echo ""
 node appController.js
