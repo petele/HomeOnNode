dbda36fb760e125aa70bea85a4cd6a55cc0399c6
Pete LePage <pete.lepage@pobox.com>
2016-04-11 18:02:45 -0400

    GCM with data

--- Web/app/sw-notifications.js
+++ Web/app/sw-notifications.js
@@ -2,11 +2,26 @@
 
 self.addEventListener('push', function(event) {
   console.log('Push message', event);
-  var title = 'HomeOnNode';
+  var notTitle = 'HomeOnNode';
+  var notBody = 'Something happened!';
+  var notIcon = 'images/athome-192.png';
+  var notTag = 'my-tag';
+  if (event.data) {
+    var data = {};
+    try {
+      data = event.data.json();
+    } catch (ex) {
+      console.log('Push Event Listener failed:', ex);
+    }
+    notTitle = data.title || 'HomeOnNode';
+    notBody = data.body || 'Something happened!';
+    notIcon = data.icon || 'images/athome-192.png';
+    notTag = data.tag || 'my-tag';
+  }
   event.waitUntil(
-    self.registration.showNotification(title, {
-      body: 'Something happened!',
-      icon: 'images/athome-192.png',
-      tag: 'my-tag'
+    self.registration.showNotification(notTitle, {
+      body: notBody,
+      icon: notIcon,
+      tag: notTag
     }));
 });
--- app/GCMPush.js
+++ app/GCMPush.js
@@ -4,7 +4,8 @@ var EventEmitter = require('events').EventEmitter;
 var Keys = require('./Keys').keys;
 var util = require('util');
 var log = require('./SystemLog');
-var request = require('request');
+// var request = require('request');
+// var webpush = require('web-push-encryption');
 
 function GCMPush(fb) {
   var _fb = fb;
@@ -33,14 +34,12 @@ function GCMPush(fb) {
       return;
     }
     _fb.child('pushSubscribers').on('value', function(snapshot) {
-      var keys = Object.keys(snapshot.val());
-      if (keys) {
-        log.debug('[GCMPush] pushSubscribers updated: ' + keys.length);
-        _subscribers = keys;
-      } else {
-        log.debug('[GCMPush] pushSubscribers updated, no subscribers');
-        _subscribers = [];
-      }
+      var subscribers = [];
+      snapshot.forEach(function(subscriber) {
+        subscribers.push(subscriber.val());
+      });
+      _subscribers = subscribers;
+      log.debug('[GCMPush] pushSubscribers updated: ' + subscribers.length);
       if (_sentReady === false) {
         log.log('[GCMPush] Ready');
         _sentReady = true;
@@ -56,46 +55,71 @@ function GCMPush(fb) {
    ****************************************************************************/
 
   this.send = function(callback) {
-    return this.sendMessage(null, callback);
+    log.log('[GCMPush.send] Not yet fully implemented.');
+    callback();
+    // return this.sendMessage(null, callback);
   };
 
   this.sendMessage = function(message, callback) {
-    if (message) {
-      log.warn('[GCMPush] Message not supported, sending empty message');
-    }
-    if (!_gcmReq.headers.Authorization) {
-      log.error('[GCMPush] No authorization available, aborting.');
-      return {error: true, message: 'No authorization'};
-    }
-    if (_subscribers.length === 0) {
-      log.warn('[GCMPush] No subscribers');
-      return {error: true, message: 'No subscribers'};
-    }
-    /* jshint -W106 */
-    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-    _gcmReq.body = {registration_ids: _subscribers};
-    // jscs:enable
-    /* jshint +W106 */
-    var msg = '[GCMPush] Sending message to ' + _subscribers.length;
-    msg += ' devices';
-    log.debug(msg);
-    request(_gcmReq, function(error, response, body) {
-      if (error) {
-        log.exception('[GCMPush] Failed', error);
-      } else if (response && response.statusCode !== 200) {
-        log.error('[GCMPush] Status code error (' + response.statusCode + ')');
-      } else if (body.failure || body.error) {
-        msg = '[GCMPush] Error sending some messages: ' + JSON.stringify(body);
-        log.error(msg);
-      } else {
-        log.log('[GCMPush] Sent (' + body.success + ') messages.');
-      }
-      if (callback) {
-        callback(body);
-      }
-    });
-    return true;
+    log.log('[GCMPush.sendMessage] Not yet fully implemented.');
+    callback();
+    // _subscribers.forEach(function(subscriber) {
+    //   var endPoint = subscriber.endPoint;
+    //   if (endPoint.keys) {
+    //     message = JSON.stringify(message);
+    //     webpush.sendWebPush(message, endPoint, Keys.gcm.apiKey)
+    //       .then(function(resp) {
+    //         log.debug('[GCMPush] ' + JSON.stringify(resp));
+    //       })
+    //       .catch(function(resp) {
+    //         log.error('[GCMPush] ' + JSON.stringify(resp));
+    //       });
+    //   } else {
+    //     var msg = '[GCMPush] Unable to send, no keys for end point: ';
+    //     msg += endPoint.endpoint;
+    //     log.warn(msg);
+    //   }
+    // });
+    // callback();
   };
+
+  // this.sendMessage = function(message, callback) {
+  //   if (message) {
+  //     log.warn('[GCMPush] Message not supported, sending empty message');
+  //   }
+  //   if (!_gcmReq.headers.Authorization) {
+  //     log.error('[GCMPush] No authorization available, aborting.');
+  //     return {error: true, message: 'No authorization'};
+  //   }
+  //   if (_subscribers.length === 0) {
+  //     log.warn('[GCMPush] No subscribers');
+  //     return {error: true, message: 'No subscribers'};
+  //   }
+  //   /* jshint -W106 */
+  //   // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+  //   _gcmReq.body = {registration_ids: _subscribers};
+  //   // jscs:enable
+  //   /* jshint +W106 */
+  //   var msg = '[GCMPush] Sending message to ' + _subscribers.length;
+  //   msg += ' devices';
+  //   log.debug(msg);
+  //   request(_gcmReq, function(error, response, body) {
+  //     if (error) {
+  //       log.exception('[GCMPush] Failed', error);
+  //     } else if (response && response.statusCode !== 200) {
+  //       log.error('[GCMPush] Status code error (' + response.statusCode + ')');
+  //     } else if (body.failure || body.error) {
+  //       msg = '[GCMPush] Error sending some messages: ' + JSON.stringify(body);
+  //       log.error(msg);
+  //     } else {
+  //       log.log('[GCMPush] Sent (' + body.success + ') messages.');
+  //     }
+  //     if (callback) {
+  //       callback(body);
+  //     }
+  //   });
+  //   return true;
+  // };
 
   init();
 }
