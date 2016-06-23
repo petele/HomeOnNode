3c81490fa06148ae1a1f34056cc5ac1a14e350c8
Pete LePage <pete.lepage@pobox.com>
2016-06-22 18:24:20 -0700

    a

--- app/Sonos.js
+++ app/Sonos.js
@@ -26,40 +26,13 @@ function Sonos() {
       return result.trim();
     },
     info: function() {
-      var result = '';
-      var args = Array.prototype.slice.call(arguments);
-      args.forEach(function(arg) {
-        if (typeof arg === 'string') {
-          result += arg + ' ';
-        } else {
-          result += util.inspect(arg) + ' ';
-        }
-      });
-      log.info('[SONOS*]' + result);
+      log.log('[SONOS*]' + this.stringify(arguments));
     },
     error: function() {
-      var result = '';
-      var args = Array.prototype.slice.call(arguments);
-      args.forEach(function(arg) {
-        if (typeof arg === 'string') {
-          result += arg + ' ';
-        } else {
-          result += util.inspect(arg) + ' ';
-        }
-      });
-      log.error('[SONOS*] ' + result);
+      log.error('[SONOS*] ' + this.stringify(arguments));
     },
     debug: function() {
-      var result = '';
-      var args = Array.prototype.slice.call(arguments);
-      args.forEach(function(arg) {
-        if (typeof arg === 'string') {
-          result += arg + ' ';
-        } else {
-          result += util.inspect(arg) + ' ';
-        }
-      });
-      log.debug('[SONOS*] ' + result);
+      log.debug('[SONOS*] ' + this.stringify(arguments));
     }
   };
 
