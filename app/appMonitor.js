65bac17e63742b2d97db2be5c918c3ed7a751a1e
Pete LePage <pete.lepage@pobox.com>
2016-06-27 17:58:12 -0400

    updated forever monitor

--- app/appMonitor.js
+++ app/appMonitor.js
@@ -4,30 +4,34 @@ var os = require('os');
 var Firebase = require('firebase');
 var exec = require('child_process').exec;
 var Keys = require('./Keys').keys;
-var log = require('./SystemLog');
+var log = require('./SystemLog2');
 
 var fbURL = 'https://' + Keys.firebase.appId + '.firebaseio.com/';
 var fb = new Firebase(fbURL);
 var fbNode;
 var deviceName = os.hostname();
 
-log.appStart(deviceName);
+var LOG_PREFIX = 'MONITOR';
+var logOpts = {
+  logFileName: './logs/system.log',
+  logToFile: true
+};
+log.appStart(deviceName, logOpts);
 
 fb.authWithCustomToken(Keys.firebase.key, function(error, authToken) {
   if (error) {
-    log.exception('Auth failed.', error);
+    log.exception(LOG_PREFIX, 'Firebase auth failed.', error);
   } else {
-    log.log('Auth success.');
+    log.log(LOG_PREFIX, 'Firebase auth success.');
     fbReady();
   }
 });
 
 function fbReady() {
-  
   fbNode = fb.child('monitor/' + deviceName);
   fb.child('.info/connected').on('value', function(snapshot) {
     if (snapshot.val() === true) {
-      log.log('Connected.');
+      log.log(LOG_PREFIX, 'Connected to Firebase');
       fbNode.child('alive').set(true);
       fbNode.child('alive').onDisconnect().set(false);
       fbNode.child('reboot').set(false);
@@ -38,7 +42,7 @@ function fbReady() {
       fbNode.child('offlineAt')
         .onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
     } else {
-      log.warn('Disconnected.');
+      log.warn(LOG_PREFIX, 'Disconnected from Firebase');
     }
   });
   fbNode.child('reboot').on('value', rebootRequest);
@@ -51,8 +55,12 @@ function heartbeat() {
 
 function rebootRequest(snapshot) {
   if (snapshot.val() === true) {
-    log.log('Reboot requested.');
+    log.log(LOG_PREFIX, 'Reboot requested.');
     var cmd = 'sudo reboot';
     exec(cmd, function(error, stdout, stderr) {});
   }
 }
+
+setInterval(function() {
+  log.cleanFile(logOpts.logFileName);
+}, 60 * 60 * 24 * 1000);
