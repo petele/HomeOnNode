654a37d57a838bb892390d51e7517a2df69fca05
Pete LePage <pete.lepage@pobox.com>
2016-08-01 16:57:02 -0400

    alarms update

--- tools/addAlarms.js
+++ tools/addAlarms.js
@@ -4,11 +4,24 @@ var Keys = require('../app/Keys').keys;
 var Firebase = require('../app/node_modules/firebase');
 var log = require('../app/SystemLog2');
 
+// {name: 'Weekday Morning Recurring', scheduledFor: 'R0111110T09:30', lastFired: 0, timesFired: 0, enabled: true},
+// {name: 'Weekday Afternoon Recurring', scheduledFor: 'R0111110T14:48', lastFired: 0, timesFired: 0, enabled: true},
+// {name: 'Daily Morning', scheduledFor: 'T09:48', lastFired: 0, timesFired: 0, enabled: true},
+// {name: 'Daily Afternoon', scheduledFor: 'T15:48', lastFired: 0, timesFired: 0, enabled: true},
+
+
 var myAlarms = [
-{name: 'Weekday Morning', time: 'R0111110T09:30:00', lastFired: 0, timesFired: 0, enabled: true},
-{name: 'OneTime A', time: '2016-08-01T10:28:00.000', lastFired: 0, timesFired: 0, enabled: true},
-{name: 'OneTime B', time: '2016-08-01T10:37:00.000', lastFired: 0, timesFired: 0, enabled: true},
-{name: 'Afternoon Morning', time: 'R0111110T14:48:20', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'Daily Morning', scheduledFor: 'T09:48', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'Daily Afternoon', scheduledFor: 'T18:48', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'Disabled Daily Afternoon', scheduledFor: 'T16:48', lastFired: 0, timesFired: 0, enabled: false},
+
+{name: 'OneTimeA', scheduledFor: '2016-08-02T10:28:00', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'OneTimeB', scheduledFor: '2016-08-01T15:37:00', lastFired: 0, timesFired: 0, enabled: true},
+
+{name: 'Weekday Morning Recurring', scheduledFor: 'R0111110T09:30', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'Weekday Afternoon Recurring', scheduledFor: 'R0111110T14:48', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'Weekend Morning Recurring', scheduledFor: 'R1000001T14:48', lastFired: 0, timesFired: 0, enabled: true},
+{name: 'DisabledWeekend Morning Recurring', scheduledFor: 'R1000001T14:48', lastFired: 0, timesFired: 0, enabled: false},
 ];
 
 function addAlarms(alarms) {
--- tools/testAlarm.js
+++ tools/testAlarm.js
@@ -5,85 +5,129 @@ var Firebase = require('../app/node_modules/firebase');
 var moment = require('../app/node_modules/moment');
 var log = require('../app/SystemLog2');
 
-var DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSS';
+var DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
 var LOG_PREFIX = 'ALARM_CLOCK';
 var alarmList = {};
 
-function alarmUpdated(alarm) {
-  var key = alarm.key();
-  alarmList[key] = alarm.val();
-  var msg = 'alarmUpdated [' + key + '] for ' + alarmList[key].time;
-  log.log(LOG_PREFIX, msg);
+function updateAlarmEntry(key, alarm) {
+  var msg = 'updateEntry ' + alarm.name + ': ';
+  if (alarm.enabled === false) {
+    if (alarmList[key]) {
+      delete alarmList[key];
+    }
+    log.log(LOG_PREFIX, msg + ' DISABLED');
+    return;
+  }
+  var nextAlarm = getNextAlarm(alarm.scheduledFor);
+  if (nextAlarm.isValid === false) {
+    msg += 'INVALID';
+    log.error(LOG_PREFIX, msg, nextAlarm.creationData());
+    return;
+  }
+  if (nextAlarm.isBefore(moment())) {
+    msg += 'DISABLED (In the past: ' + nextAlarm.format(DATE_FORMAT) + ')';
+    log.warn(LOG_PREFIX, msg);
+    return;
+  }
+  alarm.scheduledAt = nextAlarm;
+  alarmList[key] = alarm;
+  log.log(LOG_PREFIX, msg + 'Set for ' + nextAlarm.format(DATE_FORMAT));
 }
+
+function alarmUpdated(fbAlarm) {
+  var key = fbAlarm.key();
+  var alarm = fbAlarm.val();
+  // var msg = 'alarmUpdated (' + key + ')';
+  // log.debug(LOG_PREFIX, msg);
+  updateAlarmEntry(key, alarm);
+}
+
 function alarmRemoved(alarm) {
   var key = alarm.key();
   delete alarmList[key];
   log.log(LOG_PREFIX, 'alarmRemoved' + key);
 }
 
-function fireAlarm(key, alarm) {
+function getNextAlarm(scheduledFor) {
   var now = moment();
-  alarm.lastFired = now.format(DATE_FORMAT);
-  alarm.timesFired += 1;
-  if (alarm.time[0] !== 'R') {
-    alarm.enabled = false;
+  var result;
+
+  if (scheduledFor[0] === 'R') {
+    var today = now.day();
+    var scheduledDays = scheduledFor.substring(1, 8);
+    if (scheduledDays === '0000000') {
+      return moment.invalid();
+    }
+    var timeScheduledFor = scheduledFor.substring(scheduledFor.indexOf('T'));
+    result = moment(timeScheduledFor, ['THH:mm', 'THH:mm:ss']);
+    if (result.isBefore(now) || scheduledDays[today] === '0') {
+      for (var i = 1; i <= 7; i++) {
+        var pointer = (today + i) % 7;
+        if (scheduledDays[pointer] === '1') {
+          result.add(today + i - 1, 'days');
+          break;
+        }
+      }
+    }
   }
-  var msg = 'alarmFired [' + key + '] at ' + alarm.lastFired;
-  log.log(LOG_PREFIX, msg, alarm);
-  fb.child('config/alarms/' + key).set(alarm);
+
+  if (scheduledFor[0] === 'T') {
+    result = moment(scheduledFor, ['THH:mm', 'THH:mm:ss']);
+    if (result.isBefore(now)) {
+      result = result.add(1, 'day');
+    }
+  }
+
+  if (!result) {
+    result = moment(scheduledFor, 'YYYY-MM-DDTHH:mm:ss');
+  }
+  return result;
 }
 
-function testAlarmTime(key, alarm) {
-  var momentNow = moment();
-  var momentAlarm;
-  if (alarm.time[0] === 'R') {
-    var dayOfWeek = momentNow.day() + 1;
-    if (alarm.time[dayOfWeek] !== '1') {
-      return false;
-    }
-    var newAlarmTime = momentNow.format('YYYY-MM-DD');
-    newAlarmTime += alarm.time.substr(alarm.time.indexOf('T'));
-    momentAlarm = moment(newAlarmTime);
-  } else {
-    momentAlarm = moment(alarm.time);
+function testAlarm(key) {
+  var now = moment();
+  var msg = 'testAlarm [' + key + '] ';
+  var alarm = alarmList[key];
+  if (!alarm) {
+    log.warn(LOG_PREFIX, msg + 'KEY not found.');
+    return false;
   }
-
   // Bails if the lastFired is the same/after the scheduled Alarm
   if (alarm.lastFired) {
     var momentLastFired = moment(alarm.lastFired);
-    if (momentLastFired.isSameOrAfter(momentAlarm)) {
+    if (momentLastFired.isSameOrAfter(alarm.scheduledAt)) {
       return false;
     }
   }
-
   // Bails if the scheduled Alarm should have fired 5+ minutes ago.
-  if (momentAlarm.diff(momentNow, 'minutes') <= -5) {
+  if (alarm.scheduledAt.diff(now, 'minutes') <= -5) {
     return false;
   }
-
-  if (momentNow.isSameOrAfter(momentAlarm, 'second')) {
+  if (now.isSameOrAfter(alarm.scheduledAt, 'second')) {
+    fireAlarm(key, alarm);
     return true;
   }
 
   return false;
 }
 
-function alarmTick(key) {
-  var alarm = alarmList[key];
-  if (alarm.enabled !== true) {
-    return false;
-  }
-  if (testAlarmTime(key, alarm) === true) {
-    fireAlarm(key, alarm);
-    return true;
-  }
-  return false;
+function fireAlarm(key, alarm) {
+  var now = moment();
+  var msg = alarm.name + '[' + key + '] fired at ';
+  msg += now.format(DATE_FORMAT);
+  msg += ' ' + alarm.scheduledFor;
+  log.log(LOG_PREFIX, msg);
+  var stats = {
+    lastFired: now.format(DATE_FORMAT),
+    timesFired: alarm.timesFired + 1
+  };
+  fb.child('config/alarms/' + key).update(stats);
 }
 
 function timerTick() {
   var keys = Object.keys(alarmList);
-  keys.forEach(alarmTick);
-  setTimeout(timerTick, 1000);
+  keys.forEach(testAlarm);
+  setTimeout(timerTick, 250);
 }
 
 var fbURL = 'https://' + Keys.firebase.appId + '.firebaseio.com/';
@@ -96,7 +140,7 @@ fb.authWithCustomToken(Keys.firebase.key, function(error, authToken) {
     fb.child('config/alarms').on('child_changed', alarmUpdated);
     fb.child('config/alarms').on('child_added', alarmUpdated);
     fb.child('config/alarms').on('child_removed', alarmRemoved);
-    addRandomAlarm();
+    addRandomAlarm('now');
   }
 });
 
@@ -104,14 +148,19 @@ timerTick();
 
 var i = 0;
 
-function addRandomAlarm() {
-  var max = 20;
-  var min = 4;
+function addRandomAlarm(when) {
+  var min = 5;
+  var max = 25;
+  if (when === 'now') {
+    min = 1;
+    max = 1;
+  }
   var randomMinute = Math.floor(Math.random() * (max - min + 1) + min);
   var timeForAlarm = moment().add(randomMinute, 'minutes');
+  timeForAlarm = timeForAlarm.milliseconds(0);
   var randAlarm = {
     name: 'Random Alarm: ' + i.toString(),
-    time: timeForAlarm.format(DATE_FORMAT),
+    scheduledFor: timeForAlarm.format('THH:mm:ss.SSS'),
     lastFired: 0,
     timesFired: 0,
     enabled: true
