80032d0fecd6477029090f3ccabd0c99c7042b64
Pete LePage <pete.lepage@pobox.com>
2017-03-23 19:19:23 -0400

    new web ui

---
+++ Web/scripts/app.js
@@ -0,0 +1,42 @@
+window.app = {};
+app.RESET_DISPLAY_AFTER = 2 * 60;
+app.lastInput = Date.now();
+app.fbRoot = new Firebase(window.fbURL);
+app.fbRoot.authWithCustomToken(window.fbKey, function(err, user) {
+  if (err) {
+    app.showToast('Firebase authentication error.');
+    console.log('Firebase Auth Error', err);
+    return;
+  }
+  app.showToast('auth')
+  app.fbRoot.child('.info/connected').on('value', function(snapshot) {
+    if (snapshot.val() === true) {
+      app.showToast('Connected to HomeOnNode');
+    } else {
+      app.showToast('ERROR: Connection to HomeOnNode lost.');
+    }
+  });
+});
+app.sendCommand = function(cmd) {
+  app.lastInput = Date.now();
+  console.log('[sendCommand]', cmd);
+  app.fbRoot.child('commands').push(cmd, function(err) {
+    if (err) {
+      console.error('[sendCommand]', cmd, err);
+    }
+  });
+};
+app.sortByName = function(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+};
+app.showToast = function(text) {
+  if (app.$ && app.$.toast) {
+    app.$.toast.text = text;
+    app.$.toast.show();
+  }
+};
