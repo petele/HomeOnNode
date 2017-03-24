bbee918f9c536fd172f6e971b3688da7d9b7ac2d
Pete LePage <pete.lepage@pobox.com>
2017-03-23 20:30:04 -0400

    log updates

--- app/cronDaily.js
+++ app/cronDaily.js
@@ -4,9 +4,10 @@
 
 var cronJob = function() {
   log.cleanLogs('logs/doors', 30);
-  log.cleanLogs('logs/logs', 4);
+  log.cleanLogs('logs/logs', 7);
   log.cleanLogs('logs/presence');
   log.cleanLogs('logs/systemState', 14);
+  log.cleanLogs('logs/pushBullet', 7);
   log.cleanFile();
 };
 
