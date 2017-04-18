ec720edef35b05b19648f303b3dfa7d2a9b6c20e
Pete LePage <pete.lepage@pobox.com>
2017-04-17 22:26:38 -0400

    files

--- Web/scripts/app.js
+++ Web/scripts/app.js
@@ -1,33 +1,5 @@
-window.app = {};
+window.app = window.app || {};
 app.lastInput = Date.now();
-app.fbRoot = new Firebase(window.fbURL);
-app.fbRoot.authWithCustomToken(window.fbKey, function(err) {
-  if (err) {
-    app.showToast('Firebase authentication error.');
-    console.error('Firebase Auth Error', err);
-    return;
-  }
-  app.fbRoot.child('.info/connected').on('value', function(snapshot) {
-    if (snapshot.val() === true) {
-      app.showToast('Connected to HomeOnNode');
-    } else {
-      app.showToast('ERROR: Connection to HomeOnNode lost.');
-    }
-  });
-  setInterval(() => {
-    app.fbRoot.child('monitor/webClients/heartbeat').set(Date.now());
-  }, 3 * 60 * 1000);
-});
-app.sendCommand = function(cmd) {
-  app.lastInput = Date.now();
-  console.log('[sendCommand]', cmd);
-  app.fbRoot.child('commands').push(cmd, function(err) {
-    if (err) {
-      console.error('[sendCommand]', cmd, err);
-      app.showToast(err.message);
-    }
-  });
-};
 app.sortByName = function(a, b) {
   if (a.name < b.name) {
     return -1;
@@ -35,10 +7,4 @@ app.sortByName = function(a, b) {
     return 1;
   }
   return 0;
-};
-app.showToast = function(text) {
-  if (app.$ && app.$.toast) {
-    app.$.toast.text = text;
-    app.$.toast.show();
-  }
 };
