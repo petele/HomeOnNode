9296a91154443dac1e525d7b362e7ca5fed8dcd4
Pete LePage <pete.lepage@pobox.com>
2017-06-28 16:46:31 -0400

    better dumplog

--- app/dumpLogs.js
+++ app/dumpLogs.js
@@ -1,8 +1,20 @@
+#!/usr/bin/env node
+
 'use strict';
 
 const Keys = require('./Keys').keys;
 const Firebase = require('firebase');
 const log = require('./SystemLog2');
+const commander = require('commander');
+
+commander
+  .version('0.2.0')
+  .option('-n, --number <value>', 'Number of log items to show', 100)
+  .option('-p, --path <value>', 'Log path to use', 'server')
+  .option('-q, --quit', 'Quit after completion')
+  .parse(process.argv);
+
+commander.number = parseInt(commander.number, 10);
 
 /**
  * Reboot the device.
@@ -10,11 +22,15 @@ const log = require('./SystemLog2');
  * @param {String} path Firebase path of the logs to print.
 */
 function printLogs(path) {
-  fb.child(path).orderByChild('date').limitToLast(100).on('child_added',
-    function(snapshot) {
+  let logItemsShown = 0;
+  fb.child(path).orderByChild('date').limitToLast(commander.number)
+    .on('child_added', function(snapshot) {
       let msg = log.stringifyLog(snapshot.val());
       // eslint-disable-next-line no-console
       console.log(msg);
+      if ((++logItemsShown >= commander.number) && (commander.quit === true)) {
+        process.exit(0);
+      }
     }
   );
 }
@@ -25,8 +41,7 @@ fb.authWithCustomToken(Keys.firebase.key, function(error, authToken) {
     log.exception('DUMPLOG', 'Firebase auth failed.', error);
   } else {
     log.log('DUMPLOG', 'Firebase auth success.');
-    let path = process.argv[2] || 'server';
-    log.log('DUMPLOG', `logs/${path}`);
-    printLogs(`logs/${path}`);
+    log.log('DUMPLOG', `logs/${commander.path}`);
+    printLogs(`logs/${commander.path}`);
   }
 });
