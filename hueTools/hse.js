ffab6de37bef9137daea68010860cb0b9795c112
Pete LePage <pete.lepage@pobox.com>
2018-01-17 17:26:57 -0500

    WIP

--- hueTools/hse.js
+++ hueTools/hse.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 'use strict';
 
-const log = require('npmlog');
+/* eslint no-console: "off", require-jsdoc: "off" */
+
+const chalk = require('chalk');
 const commander = require('commander');
 const hseLib = require('./lib/HueSceneEditor');
 
-const LOG_PREFIX = 'CLI';
+console.log(chalk.bold('HomeOnNode Hue Scene Helper'));
 
-log.log(LOG_PREFIX, 'HomeOnNode Hue Scene Helper');
+function printResult(results) {
+  console.log(results);
+}
 
 commander
   .version('0.2.0')
@@ -16,54 +20,83 @@ commander
   .option('-r, --recipeFile <filename>', 'Read [recipes.json]', 'recipes.json');
 
 commander
+  .command('recipes')
+  .description('Lists all of the possible recipes')
+  .action(() => {
+    const recipes = hseLib.getRecipes();
+    const keys = Object.keys(recipes);
+    keys.forEach(function(key) {
+      const k = chalk.cyan(key.padEnd(20, ' '));
+      console.log(k, JSON.stringify(recipes[key].hue));
+    });
+  });
+
+commander
+  .command('list')
+  .description('Lists all of the current scenes')
+  .action(() => {
+    hseLib.listScenes()
+      .then((scenes) => {
+        const keys = Object.keys(scenes);
+        keys.forEach(function(key) {
+          const k = chalk.cyan(key.padEnd(20, ' '));
+          console.log(k, scenes[key].name);
+        });
+      });
+  });
+
+commander
+  .command('test')
+  .description('Activates the specified scene.')
+  .action(() => {
+    return hseLib.test().then(printResult);
+  });
+
+commander
+  .command('activate <sceneID>')
+  .description('Activates the specified scene.')
+  .action((sceneID) => {
+    return hseLib.activateScene(sceneID).then(printResult);
+  });
+
+commander
   .command('set <filename>')
   .description('Sets all lights in a scene definition file to their settings.')
-  .action(hseLib.setLights);
+  .action((filename) => {
+    return hseLib.setLights(filename).then(printResult);
+  });
 
-// commander
-//   .command('recipes')
-//   .description('Lists all of the possible recipes')
-//   .action(() => {
-//     readRecipeFile(commander.recipeFile);
-//     const keys = Object.keys(recipes);
-//     keys.forEach(function(key) {
-//       let k = key + '                            ';
-//       k = k.substring(0, 20);
-//       // TODO
-//       // log.log(k, JSON.stringify(recipes[key]));
-//     });
-//   });
+commander
+  .command('create <filename>')
+  .description('Create a new scene based on the scene file.')
+  .action((filename) => {
+    return hseLib.createScene(filename)
+      .then(printResult)
+      .catch(printResult);
+  });
 
-// commander
-//   .command('list')
-//   .description('Lists all of the current scenes')
-//   .action(editor.getScenes)
-//   .then((sceneList) => {
-//     const keys = Object.keys(sceneList);
-//     keys.forEach((key) => {
-//       let k = key + '                            ';
-//       k = k.substring(0, 16);
-//       // TODO
-//       // log.info(k, sceneList[key].name);
-//     });
-//   });
+commander
+  .command('update <filename>')
+  .description('Update an existing scene.')
+  .action((filename) => {
+    return hseLib.updateScene(filename)
+      .then(printResult)
+      .catch(printResult);
+  });
 
-// commander
-//   .command('create <filename>')
-//   .description('Create a new scene based on the scene file.')
-//   .action(editor.createScene);
-
-// commander
-//   .command('delete <sceneID>')
-//   .description('Delete an existing scene.')
-//   .action(editor.deleteScene);
-
-// commander
-//   .command('update <filename>')
-//   .description('Update an existing scene.')
-//   .action(editor.updateScene);
+commander
+  .command('delete <sceneID>')
+  .description('Delete an existing scene.')
+  .action((sceneID) => {
+    return hseLib.deleteScene(sceneID).then(printResult);
+  });
 
 commander.parse(process.argv);
 if (commander.args.length === 0) {
   commander.help();
 }
+
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('An unhandled promise rejection occured.', reason);
+});
--- hueTools/lib/HueSceneEditor.js
+++ hueTools/lib/HueSceneEditor.js
@@ -1,59 +1,93 @@
 'use strict';
 
+/* eslint no-console: "off" */
+
 const fs = require('fs');
-// const Keys = require('../../app/Keys').keys;
+const chalk = require('chalk');
 const request = require('request');
-// const commander = require('commander');
-const log = require('npmlog');
 
-// const requestTimeout = 30000;
-// const hueIP = '192.168.86.206';
-// let recipes;
+let _recipes;
 
+/** ***************************************************************************
+ * Constants & Remark Lint Options
+ *****************************************************************************/
+
+const VERBOSE = true;
 const HUE_IP = '192.168.86.206';
 const HUE_KEY = '9075e416a7d67c2f6c7d9386dff2e591';
 const REQUEST_TIMEOUT = 30000;
+const SLEEP_BETWEEN_REQUESTS = 150;
 
-function readJSONFile(filename) {
-  return new Promise((resolve, reject) => {
-    let result;
-    try {
-      result = fs.readFileSync(filename, 'utf8');
-    } catch (ex) {
-      reject(ex);
-      return;
-    }
-    try {
-      result = JSON.parse(result);
-      resolve(result);
-    } catch (ex) {
-      reject(ex);
-    }
-    return;
-  });
+/** ***************************************************************************
+ * Internal Helper Functions
+ *****************************************************************************/
+
+/**
+ * Waits a specified number of milliseconds
+ *
+ * @param {number} ms The number of milliseconds to wait.
+ * @return {Promise} A promise that resolves after specified time.
+ */
+function wait(ms) {
+  return new Promise((r) => setTimeout(r, ms));
 }
 
-function setLights(filename) {
-  return Promise.all([readJSONFile(filename), readJSONFile('recipes.json')])
-    .then((p) => {
-      const scene = p[0];
-      const recipes = p[1];
-      const results = [];
-      scene.lights.forEach((light) => {
-        const state = {};
-        const recipe = getRecipe(light.cmdName, recipes);
-        Object.keys(recipe).forEach((k) => {
-          state[k] = recipe[k];
-        });
-        if (light.state) {
-          Object.keys(light.state).forEach((k) => {
-            state[k] = light.state[k];
-          });
-        }
-        results.push(setLight(light.id, state));
-      });
-      return Promise.all(results);
+function readJSONFile(filename) {
+  let result;
+  try {
+    result = fs.readFileSync(filename, 'utf8');
+  } catch (ex) {
+    const msg = `Unable to read ${filename}`;
+    console.error(chalk.red('ERROR:'), `${msg}\n${ex}`);
+    throw new Error(msg);
+  }
+  try {
+    result = JSON.parse(result);
+    return result;
+  } catch (ex) {
+    const msg = `Unable to parse ${filename}`;
+    console.error(chalk.red('ERROR:'), `${msg}\n${ex}`);
+    throw new Error(msg);
+  }
+}
+
+function getRecipes() {
+  if (!_recipes) {
+    _recipes = readJSONFile('recipes.json');
+  }
+  return _recipes;
+}
+
+function getLightList(lights) {
+  const lightList = [];
+  lights.forEach((light) => {
+    lightList.push(light.light);
+  });
+  return lightList;
+}
+
+async function _setLights(lights) {
+  const results = [];
+  const recipes = getRecipes();
+  for (let i = 0; i < lights.length; i++) {
+    const light = lights[i];
+    const state = {};
+    const recipe = getRecipe(light.cmdName, recipes);
+    Object.keys(recipe).forEach((k) => {
+      state[k] = recipe[k];
     });
+    Object.keys(light).forEach((k) => {
+      if (k === 'light' || k === 'cmdName') {
+        return;
+      }
+      state[k] = light[k];
+    });
+    if (i > 0) {
+      await wait(SLEEP_BETWEEN_REQUESTS);
+    }
+    results.push(await setLight(light.light, state));
+  }
+  return results;
 }
 
 function getRecipe(recipeName, recipes) {
@@ -72,220 +106,92 @@ function setLight(lightID, state) {
   return makeRequest(method, path, state);
 }
 
+function saveSceneFile(filename, sceneObj) {
+  const jsonified = JSON.stringify(sceneObj, null, 2);
+  fs.writeFileSync(filename, jsonified, 'utf8');
+  return sceneObj;
+}
 
-// function createScene(filename) {
-//   log.log(LOG_PREFIX, `Create scene from ${filename}`);
-//   const sceneObj = readSceneFile(filename);
-//   if (sceneObj.sceneId) {
-//     log.error(LOG_PREFIX, `Create failed: ${filename} already has a sceneId field.`);
-//     log.error(LOG_PREFIX, `Use 'update' instead.`);
-//     process.exit(1);
-//   }
-//   readRecipeFile(commander.recipeFile);
-//   setLightState(sceneObj.lights, function(lightList) {
-//     var appDataValue = 'HoN';
-//     if (sceneObj.showInWebUI) {
-//       appDataValue += ',UI';
-//     }
-//     var scene = {
-//       name: sceneObj.sceneName,
-//       lights: lightList,
-//       recycle: false,
-//       appdata: {data: appDataValue, version: 4}
-//     };
-//     if (sceneObj.transitionTime) {
-//       scene.transitiontime = sceneObj.transitionTime;
-//     }
-//     makeRequest('POST', 'scenes/', scene)
-//     .then(function(result) {
-//       if (result && result[0] && result[0].success) {
-//         let id = result[0].success.id;
-//         sceneObj.sceneId = id;
-//         saveSceneFile(filename, sceneObj);
-//       } else {
-//         log.warn(LOG_PREFIX, 'Scene not saved.');
-//       }
-//       return commandCompleted(result);
-//     });
-//   });
-// }
+/** ***************************************************************************
+ * Task Functions
+ *****************************************************************************/
 
-// function deleteScene(sceneID) {
-//   log.log(LOG_PREFIX, `Delete scene ${sceneID}`);
-//   makeRequest('DELETE', 'scenes/' + sceneID, null, commandCompleted);
-// }
+function setLights(filename) {
+  const sceneObj = readJSONFile(filename);
+  return _setLights(sceneObj.lights);
+}
 
-// function updateScene(filename) {
-//   log.log(LOG_PREFIX, `Update scene from ${filename}`);
-//   let sceneObj = readSceneFile(filename);
-//   if (!sceneObj.sceneId) {
-//     log.error(LOG_PREFIX, 'File is missing `sceneId` field.');
-//     return;
-//   }
-//   readRecipeFile(commander.recipeFile);
-//   setLightState(sceneObj.lights, function(lightList) {
-//     let scene = {
-//       name: sceneObj.sceneName,
-//       lights: lightList,
-//       storelightstate: true
-//     };
-//     if (sceneObj.transitionTime) {
-//       sceneObj.transitiontime = sceneObj.transitionTime;
-//     }
-//     makeRequest('PUT', 'scenes/' + sceneObj.sceneId, scene, commandCompleted);
-//   });
-// }
+function listScenes() {
+  return makeRequest('GET', 'scenes/', null);
+}
 
-// function getScenes() {
-//   makeRequest('GET', 'scenes/', null, function(sceneList) {
-//     var keys = Object.keys(sceneList);
-//     keys.forEach(function(key) {
-//       var k = key + '                            ';
-//       k = k.substring(0, 16);
-//       let sceneName = sceneList[key].name;
-//       //TODO
-//       // log.info(k, sceneName);
-//     });
-//   });
-// }
+function activateScene(sceneID) {
+  const body = {scene: sceneID};
+  return makeRequest('PUT', 'groups/0/action', body);
+}
 
+function deleteScene(sceneID) {
+  return makeRequest('DELETE', 'scenes/' + sceneID, null);
+}
 
-// function commandCompleted(body) {
-//   if (Array.isArray(body) !== true) {
-//     log.warn(LOG_PREFIX, `Expected body to be Array by it was ${typeof body}`);
-//     log.log(LOG_PREFIX, 'Complete', body);
-//     return;
-//   }
-//   body.forEach(function(resp) {
-//     if (resp.success) {
-//       log.log(LOG_PREFIX, `Command completed successfully: ${resp.success}`);
-//     } else {
-//       log.warn(LOG_PREFIX, `Command completed: ${resp}`);
-//     }
-//   });
-//   return body;
-// }
+async function createScene(filename) {
+  const sceneObj = readJSONFile(filename);
+  if (sceneObj.sceneId) {
+    return Promise.reject('Scene already exists.');
+  }
+  await _setLights(sceneObj.lights);
+  let appDataValue = 'HoN';
+  if (sceneObj.showInWebUI) {
+    appDataValue += ',UI';
+  }
+  const scene = {
+    name: sceneObj.sceneName,
+    lights: getLightList(sceneObj.lights),
+    recycle: false,
+    appdata: {data: appDataValue, version: 4},
+  };
+  if (sceneObj.transitionTime) {
+    scene.transitiontime = sceneObj.transitionTime;
+  }
+  await wait(SLEEP_BETWEEN_REQUESTS);
+  return makeRequest('POST', 'scenes/', scene)
+    .then((result) => {
+      if (result && result[0] && result[0].success) {
+        const sceneId = result[0].success.id;
+        sceneObj.sceneId = sceneId;
+        saveSceneFile(filename, sceneObj);
+        return sceneId;
+      }
+      return result;
+    })
+}
 
-// function saveSceneFile(filename, sceneObj) {
-//   log.debug(LOG_PREFIX, `Saving ${filename}`);
-//   try {
-//     let jsonified = JSON.stringify(sceneObj, null, 2);
-//     fs.writeFileSync(filename, jsonified, 'utf8');
-//     log.log(LOG_PREFIX, `${sceneObj.sceneName} as ${sceneObj.sceneId}`);
-//     return sceneObj;
-//   } catch (ex) {
-//     log.error(LOG_PREFIX, 'Error: could not write scene definition file.');
-//     //TODO
-//     process.exit(1);
-//   }
-// }
-
-// function readSceneFile(filename) {
-//   var result;
-//   try {
-//     log.debug(LOG_PREFIX, `readSceneFile: Reading ${filename}`);
-//     result = fs.readFileSync(filename);
-//   } catch (ex) {
-//     log.error('', 'Error: could not read scene definition file.');
-//     process.exit(1);
-//   }
-//   try {
-//     log.verbose('readSceneFile', 'Parsing');
-//     result = JSON.parse(result);
-//   } catch (ex) {
-//     log.error('', 'Error: could not parse scene definition file.');
-//     process.exit(1);
-//   }
-//   log.verbose('readSceneFile', 'Validating [sceneName]');
-//   if (!result.sceneName) {
-//     log.error('', 'ERROR: Scene definition file is missing sceneName');
-//     process.exit(1);
-//   }
-//   if (!result.lights) {
-//     result.lights = [];
-//   }
-//   if (result.scenes) {
-//     result.scenes.forEach(function(sceneFile) {
-//       let scene = readSceneFile(sceneFile);
-//       result.lights = result.lights.concat(scene.lights);
-//     });
-//   }
-//   log.verbose('readSceneFile', 'Validating [lights]');
-//   if (Array.isArray(result.lights) === false || result.lights.length === 0) {
-//     log.error('', 'ERROR: Scene definition file doesn\'t contain lights array');
-//     process.exit(1);
-//   }
-//   return result;
-// }
-
-// function readRecipeFile(filename) {
-//   var result = {};
-//   var scenes;
-//   try {
-//     log.verbose('readRecipeFile', 'Reading %s', filename);
-//     scenes = fs.readFileSync(filename);
-//   } catch (ex) {
-//     log.error('readRecipeFile', 'Not found, using default.');
-//     process.exit(1);
-//   }
-//   try {
-//     log.verbose('readRecipeFile', 'Parsing');
-//     result = JSON.parse(scenes);
-//   } catch (ex) {
-//     log.error('readRecipeFile', 'Could not parse receipe file.');
-//     process.exit(1);
-//   }
-//   log.verbose('readRecipeFile', 'Recipes: %s', Object.keys(result).join(', '));
-//   recipes = result;
-// }
-
-// function setLightState(lights, callback) {
-//   iterateOverLights(lights, 0, [], function(lightList) {
-//     if (callback) {
-//       callback(lightList);
-//     }
-//   });
-// }
-
-// function iterateOverLights(lights, index, lightList, callback) {
-//   var light = lights[index];
-//   if (light) {
-//     lightList.push(light.light.toString());
-//     var path = 'lights/' + light.light + '/state';
-//     var body;
-//     if (light.command) {
-//       body = light.command;
-//     } else if (light.cmdName === 'OFF') {
-//       body = {on: false};
-//     } else if (light.cmdName) {
-//       body = getRecipe(light.cmdName);
-//       body.on = true;
-//       body.bri = light.bri || 254;
-//     } else {
-//       log.error('iterateOverLights', 'Missing light command: %j', light);
-//       process.exit(1);
-//     }
-//     log.verbose('iterateOverLights', 'Setting light: %s', light.light);
-//     makeRequest('PUT', path, body, function() {
-//       iterateOverLights(lights, index + 1, lightList, callback);
-//     });
-//   } else {
-//     callback(lightList);
-//   }
-// }
-
-// function getRecipe(rName) {
-//   rName = rName.toUpperCase();
-//   var result = recipes[rName];
-//   if (result) {
-//     return result.hue;
-//   } else {
-//     log.error('getRecipe', 'Recipe not found: %s', rName);
-//     process.exit(1);
-//   }
-// }
+async function updateScene(filename) {
+  const sceneObj = readJSONFile(filename);
+  if (!sceneObj.sceneId) {
+    return Promise.reject('Scene doesn\'t exist.');
+  }
+  await _setLights(sceneObj.lights);
+  const scene = {
+    name: sceneObj.sceneName,
+    lights: getLightList(sceneObj.lights),
+    storelightstate: true,
+  };
+  if (sceneObj.transitionTime) {
+    scene.transitiontime = sceneObj.transitionTime;
+  }
+  await wait(SLEEP_BETWEEN_REQUESTS);
+  return makeRequest('PUT', `scenes/${sceneObj.sceneId}`, scene);
+}
 
 
+/**
+ * Makes a request to the Hue API
+ *
+ * @param {string} method The HTTP request method.
+ * @param {string} path The URL path to request from.
+ * @param {Object} <body> The body to send along with the request.
+ */
 function makeRequest(method, path, body) {
   return new Promise(function(resolve, reject) {
     const reqOpt = {
@@ -297,25 +203,25 @@ function makeRequest(method, path, body) {
     if (body) {
       reqOpt.body = body;
     }
-    const prefix = method + ' ' + path;
-    log.info(prefix, body);
+    console.log(method, '->', path, body);
     request(reqOpt, function(error, response, body) {
-      const respPrefix = 'RESP ' + path;
-      let result = body;
       if (error) {
-        log.error(respPrefix, error);
-        result = [{failed: error}];
-        reject(result);
-      } else {
-        log.info(respPrefix, body);
-        resolve(result);
+        console.log(method, '<-', path, chalk.red('FAILED'), '\n', error);
+        reject([{failed: error}]);
+        return;
       }
+      if (VERBOSE) {
+        console.log(method, '<-', path, body);
+      }
+      resolve(body);
     });
   });
 }
 
 exports.setLights = setLights;
-// exports.getScenes = getScenes;
-// exports.createScene = createScene;
-// exports.deleteScene = deleteScene;
-// exports.updateScene = updateScene;
+exports.getRecipes = getRecipes;
+exports.listScenes = listScenes;
+exports.createScene = createScene;
+exports.deleteScene = deleteScene;
+exports.updateScene = updateScene;
+exports.activateScene = activateScene;
