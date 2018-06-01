85f94169481876bf6109981930a82108855cd583
Pete LePage <pete.lepage@pobox.com>
2018-06-01 10:21:29 -0400

    WIP

--- app/appGPIO.js
+++ app/appGPIO.js
@@ -17,6 +17,9 @@ let _wsClient;
 let _deviceMonitor;
 let _hasReadFBConfig = false;
 
+const EDGES = ['none', 'rising', 'falling', 'both'];
+
+// Read config file
 try {
   console.log(`Reading 'config.json'...`);
   let config = fs.readFileSync('config.json', {encoding: 'utf8'});
@@ -28,12 +31,16 @@ try {
   process.exit(1);
 }
 
+// Verify config has appName
+if (!_config.appName) {
+  console.error(`'appName' not set in config.`);
+  process.exit(1);
+}
 const APP_NAME = _config.appName;
+
+// Setup logging
 log.setAppName(APP_NAME);
-log.setOptions({
-  firebaseLogLevel: _config.logLevel || 50,
-  firebasePath: `logs/${APP_NAME}`
-});
+log.setOptions({firebasePath: `logs/${APP_NAME}`});
 log.startWSS();
 log.appStart();
 
@@ -60,21 +67,27 @@ function init() {
     _deviceMonitor.shutdown('FB', 'shutdown_request', 0);
   });
 
+  // Load GPIO
   if (_loadGPIO() !== true) {
     _close();
     _deviceMonitor.shutdown(APP_NAME, 'GPIO unavailable', 1);
     return;
   }
 
-  _config.pins.forEach(_registerPin);
-
+  // Connect to the web socket server
   if (_config.wsServer) {
     _wsClient = new WSClient(_config.wsServer, true);
   }
+
+  // Initialize the GCM client
   if (_config.disableGCM !== true) {
     _gcmPush = new GCMPush(_fb);
   }
 
+  // Register listeners on each pin
+  _config.pins.forEach(_registerPin);
+
+  // Listen for changes to config
   _fb.child(`config/${APP_NAME}`).on('value', (snapshot) => {
     if (_hasReadFBConfig === false) {
       _hasReadFBConfig = true;
@@ -84,9 +97,10 @@ function init() {
     fs.writeFileSync('config.json', config, {encoding: 'utf8'});
     log.log(APP_NAME, 'Config updated, restart required.');
     _close();
-    _deviceMonitor.restart('Self', 'Config changed', false);
+    _deviceMonitor.restart(APP_NAME, 'Config changed', false);
   });
 
+  // Clean up logs every 24 hours
   setInterval(function() {
     log.cleanFile();
   }, 60 * 60 * 24 * 1000);
@@ -103,7 +117,7 @@ function _loadGPIO() {
     GPIO = require('onoff').Gpio;
     return true;
   } catch (ex) {
-    log.exception(APP_NAME, 'Node module `onoff` is not available.', ex);
+    log.exception(APP_NAME, `Node module 'onoff' is not available.`, ex);
     return false;
   }
 }
@@ -112,19 +126,33 @@ function _loadGPIO() {
  * Event handler to register a pin.
  *
  * @param {Object} details Pin information details
+ * @return {Boolean} true if successfully registered pin
  */
 function _registerPin(details) {
-  if (details.disabled) {
-    log.log(APP_NAME, `Pin '${details.pinNumber}' disabled`);
-    return;
+  const pinNumber = details.pinNumber;
+  if (!pinNumber) {
+    log.error(APP_NAME, `Invalid pin number`, details);
+    return false;
   }
-  log.log(APP_NAME, `Listening on pin #${details.pinNumber}`);
+  if (details.disabled) {
+    log.log(APP_NAME, `Pin '${pinNumber}' disabled`);
+    return false;
+  }
+  const edge = details.edge;
+  if (edge && !EDGES.contains(edge)) {
+    log.error(APP_NAME, `Invalid edge (${edge}) for pin ${pinNumber}`);
+    return false;
+  }
+  log.log(APP_NAME, `Listening on pin ${pinNumber}`);
   details.lastPushed = 0;
-  const pin = new GPIO(details.pinNumber, 'in', details.edge);
-  details.lastValue = pin.readSync();
+  const pin = new GPIO(pinNumber, 'in', edge);
+  const value = pin.readSync();
+  details.lastValue = value;
+  log.debug(APP_NAME, `Pin ${pinNumber} is currently: ${value}`)
   pin.watch((err, val) => {
     _pinChanged(details, err, val);
   });
+  return true;
 }
 
 /**
@@ -136,7 +164,7 @@ function _registerPin(details) {
  */
 function _pinChanged(pin, err, value) {
   if (err) {
-    const msg = `Error on pin changed for pin ${details.pinNumber}`;
+    const msg = `Error on pin changed for pin ${pin.pinNumber}`;
     log.error(APP_NAME, msg, err);
     return;
   }
@@ -154,17 +182,23 @@ function _pinChanged(pin, err, value) {
     let command;
     let message;
     if (value === 1) {
-      command = pin.commandOpen;
-      message = pin.messageOpen;
+      command = pin.cmdTrue;
+      message = pin.msgTrue;
     } else if (value === 0) {
-      command = pin.commandClosed;
-      message = pin.messageClosed;
+      command = pin.cmdFalse;
+      message = pin.msgFalse;
     }
     if (command && _wsClient) {
-      _wsClient.send(JSON.stringify(command));
+      _wsClient.send(JSON.stringify(command)).catch((err) => {
+        log.error(APP_NAME, `Unable to send command`, err);
+        log.debug(APP_NAME, `Command sent`, command);
+      });
     }
     if (message && _gcmPush) {
-      _gcmPush.sendMessage(message);
+      _gcmPush.sendMessage(message).catch((err) => {
+        log.error(APP_NAME, `Unable to send message`, err);
+        log.debug(APP_NAME, `Message sent`, message);
+      });
     }
   }
 }
@@ -177,8 +211,8 @@ function _close() {
   if (GPIO) {
     log.debug(APP_NAME, 'Unwatching pins');
     GPIO.unwatchAll();
-    log.debug(APP_NAME, 'Unexporting GPIO');
-    GPIO.unexport();
+    // log.debug(APP_NAME, 'Unexporting GPIO');
+    // GPIO.unexport();
   }
   if (_wsClient) {
     _wsClient.shutdown();
@@ -186,6 +220,7 @@ function _close() {
 }
 
 process.on('SIGINT', function() {
+  log.log(APP_NAME, 'SigInt received, shutting down...');
   _close();
   _deviceMonitor.shutdown('SIGINT', 'shutdown_request', 0);
 });
