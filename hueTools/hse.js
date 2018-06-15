d2cc8e8e5d14dbfd05566181b251dcee81978ebc
Pete LePage <pete.lepage@pobox.com>
2018-06-14 23:13:31 -0400

    Lights Update

--- hueTools/hse.js
+++ hueTools/hse.js
@@ -12,6 +12,7 @@ const Keys = require('../app/Keys.js').keys;
 
 console.log(chalk.bold('HomeOnNode Hue Scene Helper'));
 
+const RE_ROOM_ID = /_r(\d\d)/;
 const DELAY_BETWEEN_EXTRA = 60;
 const DELAY_BETWEEN_REQUESTS = 350;
 const DELAY_BETWEEN_ACTIONS = 1500;
@@ -20,6 +21,12 @@ const UTIL_OPTS = {
   colors: true,
   maxArrayLength: 20,
   breakLength: 1000,
+};
+const UTIL_OPTS_PRETTY = {
+  colors: true,
+  compact: false,
+  depth: 4,
+  maxArrayLength: 50,
 };
 
 function initHSE() {
@@ -34,7 +41,7 @@ function initHSE() {
 }
 
 commander
-  .version('0.8.1')
+  .version('0.8.6')
   .option('-a, --address <address>', 'Address to use')
   .option('-k, --key <key>', 'Hue Key')
   .option('-i, --insecure', 'Insecure, use HTTP only')
@@ -61,80 +68,41 @@ commander
     initHSE();
     hseLib.getScene(sceneID)
       .then((scene) => {
-        const util_opts = {
-          colors: true,
-          compact: false,
-          depth: 3,
-          maxArrayLength: 50,
-        };
-        console.log(util.inspect(scene, util_opts));
+        console.log(util.inspect(scene, UTIL_OPTS_PRETTY));
       });
+  });
+
+commander
+  .command('rename <sceneID> <name>')
+  .description('Renames a scene.')
+  .action((sceneID, sceneName) => {
+    initHSE();
+    return hseLib.renameScene(sceneID, sceneName).then((results) => {
+      hseLib.printResults(results);
+    });
   });
 
 commander
   .command('save <sceneID> <filename>')
   .description('Actives scene and saves all info to a file.')
   .action((sceneID, filename) => {
-    let sceneInfo;
-    console.log('save', sceneID, filename);
     initHSE();
-    return hseLib.activateScene(sceneID).then((results) => {
-      hseLib.printResults(results);
-    }).then(() => {
-      return hseLib.getScene(sceneID);
-    }).then((scene) => {
-      console.log('sceneInfo', util.inspect(scene, UTIL_OPTS));
-      sceneInfo = scene;
-      const results = [];
-      sceneInfo.lights.forEach((light) => {
-        const p = Promise.resolve({});
-        results.push(p);
-      });
-      // loop through lights to get individual light state
-      return Promise.all(results);
-    }).then((lightStates) => {
-      const results = {
-        sceneName: sceneInfo.name,
-        sceneId: sceneID,
-        sceneData: sceneInfo.appdata,
-        roomId: 1,
-        lights: [],
-      };
-      lightStates.forEach((lightObj) => {
-        console.log('light', util.inspect(lightObj, UTIL_OPTS));
-        const light = {
-          lightId: 1,
-          command: {on: false},
-        };
-        if (lightObj.state.on) {
-          light.command.on = true;
-          light.command.bri = lightObj.state.bri;
-          if (lightObj.state.colormode === 'xy') {
-            light.command.xy = lightObj.state.xy;
-          } else if (lightObj.state.colormode === 'hs') {
-            light.command.hue = lightObj.state.hue;
-            light.command.sat = lightObj.state.sat;
-          } else if (lightObj.state.colormode === 'ct') {
-            light.command.ct = lightObj.state.ct;
-          } else {
-            console.error('OOOPS', lightObj.state.colormode);
-          }
-          if (lightObj.state.alert !== 'none') {
-            light.command.alert = lightObj.state.alert;
-          }
-          if (lightObj.state.effect !== 'none') {
-            light.command.effect = lightObj.state.effect;
-          }
-        }
-        results.lights.push(light);
-      });
-      return results;
-    }).then((sceneObj) => {
-      console.log(util.inspect(sceneObj, UTIL_OPTS));
-      // hseLib.saveJSONFile(filename, sceneObj);
+    return backupScene(sceneID, filename).then((result) => {
+      console.log(util.inspect(result, UTIL_OPTS_PRETTY));
     });
   });
 
+commander
+  .command('backup')
+  .description('Lists all of the current scenes')
+  .action(() => {
+    initHSE();
+    hseLib.listScenes()
+      .then((scenes) => {
+        const keys = Object.keys(scenes);
+        backupScenes(keys);
+      });
+  });
 
 commander
   .command('list')
@@ -146,8 +114,21 @@ commander
         const keys = Object.keys(scenes);
         keys.forEach(function(key) {
           const k = key.padEnd(16, ' ');
-          const a = scenes[key].appdata.data.padEnd(14, ' ');
-          const n = scenes[key].name;
+          let a = '';
+          if (scenes[key].appdata && scenes[key].appdata.data) {
+            a = scenes[key].appdata.data;
+          }
+          a = a.padEnd(14, ' ')
+          // try {
+          //   a = scenes[key].appdata.data.padEnd(14, ' ');
+          // } catch (ex) {
+          //   console.log('err', scenes[key]);
+          // }
+          let n = scenes[key].name;
+          // let v = scenes[key].version;
+          if (scenes[key].version < 2) {
+            n = `${n} ${chalk.red('(v1)')}`;
+          }
           console.log(chalk.cyan(k), chalk.magenta(a), n);
         });
       });
@@ -206,6 +187,47 @@ function allOffAndWait() {
       console.log(' Waiting for lights to catch up...');
       return hseLib.wait(DELAY_BETWEEN_ACTIONS);
     });
+}
+
+async function backupScenes(scenes) {
+  for (let i = 0; i < scenes.length; i++) {
+    await hseLib.wait(DELAY_BETWEEN_REQUESTS);
+    const sceneID = scenes[i];
+    const filename = `backup/${sceneID}.json`;
+    backupScene(sceneID, filename);
+  }
+}
+
+async function backupScene(sceneID, filename) {
+  const scene = await hseLib.getScene(sceneID);
+  const results = {
+    sceneName: scene.name,
+    sceneId: sceneID,
+    sceneData: scene.appdata,
+    sceneVersion: scene.version,
+  };
+  if (scene.appdata && scene.appdata.data) {
+    const matches = scene.appdata.data.match(RE_ROOM_ID);
+    if (matches && matches[1]) {
+      results.room = {id: parseInt(matches[1])};
+    }
+  }
+  results.lights = [];
+  Object.keys(scene.lightstates).forEach((lightId) => {
+    const lightState = scene.lightstates[lightId];
+    results.lights.push({
+      lightId: parseInt(lightId),
+      command: lightState,
+    });
+  });
+  let cSceneID = chalk.cyan(sceneID);
+  if (scene.version < 2) {
+    cSceneID = chalk.red(sceneID);
+  }
+  const cFilename = chalk.cyan(filename);
+  console.log(`Saved ${cSceneID} to ${cFilename}`);
+  hseLib.saveJSONFile(filename, results);
+  return results;
 }
 
 async function setScene(sceneObj) {
