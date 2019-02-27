bc13ec44ddaaf994ae38f2e6bbd80aa9360814c6
Pete LePage <pete.lepage@pobox.com>
2019-02-27 10:10:34 -0500

    app monitor updates

--- app/appMonitor.js
+++ app/appMonitor.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const os = require('os');
 const Keys = require('./Keys').keys;
 const log = require('./SystemLog2');
@@ -7,15 +8,15 @@ const Firebase = require('firebase');
 const DeviceMonitor = require('./DeviceMonitor');
 
 const LOG_PREFIX = 'MONITOR';
+const HOST_NAME = _getHostname();
 
 let _fb;
-let _hostname;
 let _deviceMonitor;
 
 const logOpts = {
-  consoleLogLevel: 20,
+  consoleLogLevel: 0,
   firebaseLogLevel: 45,
-  firebasePath: 'logs/monitor',
+  firebasePath: `logs/monitor/${HOST_NAME}`,
   fileLogLevel: 45,
   fileFilename: './logs/monitor.log',
 };
@@ -24,6 +25,19 @@ log.setAppName(LOG_PREFIX);
 log.setOptions(logOpts);
 log.startWSS(8882);
 log.appStart();
+
+/**
+ * Gets the simple hostname
+ *
+ * @return {String} hostname
+ */
+function _getHostname() {
+  const hostname = os.hostname();
+  if (!hostname.includes('.')) {
+    return hostname;
+  }
+  return hostname.substring(0, hostname.indexOf('.'));
+}
 
 /**
  * Init
@@ -39,12 +53,7 @@ function init() {
   });
   log.setFirebaseRef(_fb);
 
-  _hostname = os.hostname();
-  if (_hostname.indexOf('.') >= 0) {
-    _hostname = _hostname.substring(0, _hostname.indexOf('.'));
-  }
-
-  _deviceMonitor = new DeviceMonitor(_fb.child('monitor'), _hostname);
+  _deviceMonitor = new DeviceMonitor(_fb.child('monitor'), HOST_NAME);
   _deviceMonitor.on('restart_request', () => {
     _deviceMonitor.restart('FB', 'restart_request', false);
   });
@@ -52,9 +61,19 @@ function init() {
     _deviceMonitor.restart('FB', 'connection_timedout', false);
   });
 
-  setInterval(function() {
-    log.cleanFile();
-    log.cleanLogs(logOpts.firebasePath, 7);
+  setInterval(() => {
+    log.cleanLogs(logOpts.firebasePath, 7).catch((err) => {
+      log.exception(LOG_PREFIX, 'Unable to clean firebase logs.', err);
+    });
+    log.cleanFile().catch((err) => {
+      log.exception(LOG_PREFIX, 'Unable to clean log file.', err);
+    });
+    const foreverLogFile = './logs/forever.log';
+    if (fs.existsSync(foreverLogFile)) {
+      log.cleanFile(foreverLogFile).catch((err) => {
+        log.exception(LOG_PREFIX, 'Unable to clean forever file.', err);
+      });
+    }
   }, 60 * 60 * 24 * 1000);
 }
 
