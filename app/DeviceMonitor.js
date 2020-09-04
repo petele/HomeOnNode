7e8a415b8309bd62cd5d9b977d9a905cf398e771
petele <pete.lepage@pobox.com>
2020-09-04 12:04:54 -0400

    WIP

--- app/DeviceMonitor.js
+++ app/DeviceMonitor.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* node14_ready */
+
 const os = require('os');
 const util = require('util');
 const log = require('./SystemLog2');
--- app/Keypad.js
+++ app/Keypad.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* node14_ready */
+
 const log = require('./SystemLog2');
 const keypress = require('keypress');
 
