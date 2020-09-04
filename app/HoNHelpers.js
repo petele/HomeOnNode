cef7390d3b8d994e6bb4dac4172a57b6eaee65ff
petele <pete.lepage@pobox.com>
2020-09-04 14:43:15 -0400

    WIP

---
+++ app/HoNHelpers.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* node14_ready */
+
+
+/**
+ * Returns after specified seconds, defaults to 30.
+ *
+ * @param {Number} [seconds] Number of seconds to wait (optional).
+ * @return {?Promise<undefined>} A promise that resolves after specified time.
+ */
+function _promiseSleep(seconds) {
+  seconds = seconds || 30;
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+/**
+ * Checks if the input is a valid integer between min and max.
+ *
+ * @param {*} val
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Boolean}
+ */
+function _isValidInt(val, min, max) {
+  val = parseInt(val);
+  if (!Number.isInteger(val)) {
+    return null;
+  }
+  if (val > max) {
+    return null;
+  }
+  if (val < min) {
+    return null;
+  }
+  return val;
+}
+
+
+exports.promiseSleep = _promiseSleep;
+exports.isValidInt = _isValidInt;
