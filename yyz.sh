53f479c525357d4758f89b909e5744d04cd2a921
petele <pete.lepage@pobox.com>
2020-09-12 14:54:34 -0400

    WIP

---
+++ yyz.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Starting Toronto..."
+echo ""
+
+export NODE_NO_WARNINGS=1
+
+cd ~/HomeOnNode/app
+
+echo "Configuring Node..."
+. ~/.nvm/nvm.sh
+nvm use
+echo ""
+
+if [ ! -f ./flic.pid ]; then
+  echo "Starting Flic..."
+  forever start yyz-flic.json
+fi
+
+if [ ! -f ./gpio.pid ]; then
+  echo "Starting GPIO..."
+  forever start yyz-gpio.json
+fi
