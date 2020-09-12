759530745e91c9408d025f7ff43df2f34661a880
petele <pete.lepage@pobox.com>
2020-09-12 15:44:22 -0400

    WIP

--- yyz.sh
+++ yyz.sh
@@ -14,18 +14,21 @@ echo ""
 
 cd ~/HomeOnNode/
 
-if [ ! -f ./flic.pid ]; then
-  echo "Starting Flic..."
-  forever start --minUptime 1000 --spinSleepTime 1000 yyz-flic.json
-else
-  echo "Flic already running..."
-fi
+forever start --minUptime 1000 --spinSleepTime 1000 yyz-flic.json
+forever start --minUptime 1000 --spinSleepTime 1000 yyz-gpio.json
 
-echo ""
+# if [ ! -f ./flic.pid ]; then
+#   echo "Starting Flic..."
+#   forever start --minUptime 1000 --spinSleepTime 1000 yyz-flic.json
+# else
+#   echo "Flic already running..."
+# fi
 
-if [ ! -f ./gpio.pid ]; then
-  echo "Starting GPIO..."
-  forever start --minUptime 1000 --spinSleepTime 1000 yyz-gpio.json
-else
-  echo "GPIO already running..."
-fi
+# echo ""
+
+# if [ ! -f ./gpio.pid ]; then
+#   echo "Starting GPIO..."
+#   forever start --minUptime 1000 --spinSleepTime 1000 yyz-gpio.json
+# else
+#   echo "GPIO already running..."
+# fi
